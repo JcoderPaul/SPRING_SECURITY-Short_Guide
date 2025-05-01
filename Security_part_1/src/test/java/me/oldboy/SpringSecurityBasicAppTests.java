b9ceddb79d02c41b5659928db9a336544ace5310
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-02 00:29:05 +0500

    Add SpringSecurityBasicAppTests.java

---
+++ Security_part_1/src/test/java/me/oldboy/SpringSecurityBasicAppTests.java
@@ -0,0 +1,13 @@
+package me.oldboy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityBasicAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
