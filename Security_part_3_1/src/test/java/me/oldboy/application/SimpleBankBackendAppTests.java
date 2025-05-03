c45a473d7e5809049ae03ea90194b5920cfd0c2e
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-03 08:48:10 +0500

    Security_part_3_1: Add SimpleBankBackendAppTests.java

---
+++ Security_part_3_1/src/test/java/me/oldboy/application/SimpleBankBackendAppTests.java
@@ -0,0 +1,13 @@
+package me.oldboy.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBankBackendAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
