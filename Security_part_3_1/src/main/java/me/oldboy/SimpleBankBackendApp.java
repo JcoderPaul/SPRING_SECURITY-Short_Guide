a35565c24bb8f1c4732c159f397a0dae1def0033
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-03 08:49:07 +0500

    Security_part_3_1: Add SimpleBankBackendApp.java

---
+++ Security_part_3_1/src/main/java/me/oldboy/SimpleBankBackendApp.java
@@ -0,0 +1,17 @@
+package me.oldboy;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@Slf4j
+@SpringBootApplication
+@ComponentScan("me.oldboy")
+public class SimpleBankBackendApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBankBackendApp.class, args);
+	}
+
+}
