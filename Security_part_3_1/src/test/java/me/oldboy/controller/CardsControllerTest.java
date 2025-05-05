2bffe30b8fe2ab43416bd10681c1e14240aa4df7
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-05 20:26:05 +0500

    Security_part_3_1: Add CardsControllerTest.java

---
+++ Security_part_3_1/src/test/java/me/oldboy/controller/CardsControllerTest.java
@@ -0,0 +1,35 @@
+package me.oldboy.controller;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(CardsController.class)
+class CardsControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @SneakyThrows
+    @WithMockUser
+    void shouldReturn2xxStatusGetCardDetailsTest() {
+        mockMvc.perform(MockMvcRequestBuilders.get("/myCards"))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(content().string("Here are the card details from the DB"));
+    }
+
+    @Test
+    @SneakyThrows
+    void shouldReturn4xxStatusGetCardDetailsTest() {
+        mockMvc.perform(MockMvcRequestBuilders.get("/myCards"))
+                .andExpect(status().is4xxClientError());
+    }
+}
