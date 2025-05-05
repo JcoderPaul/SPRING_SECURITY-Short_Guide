c96ee53b1cb37deaf017d48e4e9e922b0bf76f32
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-05 14:55:04 +0500

    Security_part_2_1: Update SpringSecurityPart2AppTests.java

--- Security_part_2_1/src/test/java/me/oldboy/SpringSecurityPart2AppTests.java
+++ Security_part_2_1/src/test/java/me/oldboy/SpringSecurityPart2AppTests.java
@@ -1,13 +1,21 @@
 package me.oldboy;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class SpringSecurityPart2AppTests {
 
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(webApplicationContext);
 	}
 
 }
