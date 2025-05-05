ae4e5b5a76d40e3dbf2af7de8c6b4ab12cfaa7a6
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-05 20:25:54 +0500

    Security_part_3_1: Add BalanceControllerTest.java

---
+++ Security_part_3_1/src/test/java/me/oldboy/controller/BalanceControllerTest.java
@@ -0,0 +1,35 @@
+package me.oldboy.controller;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BalanceController.class)
+class BalanceControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @SneakyThrows
+    @WithMockUser
+    void shouldReturnOkGetBalanceDetailsTest() {
+        mockMvc.perform(MockMvcRequestBuilders.get("/myBalance"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Here are the balance details from the DB"));
+    }
+
+    @Test
+    @SneakyThrows
+    void shouldReturnNotAuthorizedGetBalanceDetailsTest() {
+        mockMvc.perform(MockMvcRequestBuilders.get("/myBalance"))
+                .andExpect(status().isUnauthorized());
+    }
+}
