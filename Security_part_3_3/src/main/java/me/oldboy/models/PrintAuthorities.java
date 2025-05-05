9ab257ff95a8cd84f858bac213c5b21b289a8c26
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-05 06:36:12 +0500

    Security_part_3_3: Add PrintAuthorities.java

---
+++ Security_part_3_3/src/main/java/me/oldboy/models/PrintAuthorities.java
@@ -0,0 +1,27 @@
+package me.oldboy.models;
+
+public class PrintAuthorities {
+    String username;
+    String authority;
+
+    public PrintAuthorities(String username, String authority) {
+        this.username = username;
+        this.authority = authority;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+}
