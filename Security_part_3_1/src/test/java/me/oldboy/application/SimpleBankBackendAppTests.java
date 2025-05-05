c2b0fee3bf3646bd789338af4902f8bde1a45f47
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-05 20:57:18 +0500

    Security_part_3_1: Edit SimpleBankBackendAppTests.java

--- Security_part_3_1/src/test/java/me/oldboy/application/SimpleBankBackendAppTests.java
+++ Security_part_3_1/src/test/java/me/oldboy/application/SimpleBankBackendAppTests.java
@@ -1,13 +1,21 @@
 package me.oldboy.application;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class SimpleBankBackendAppTests {
 
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(webApplicationContext);
 	}
 
 }
