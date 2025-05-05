b1d794048e22ee2e532e74a24115402843ad26b2
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-05 17:36:12 +0500

    Security_part_2_2: Update ContactControllerTest.java

---
+++ Security_part_2_2/src/test/java/me/oldboy/controller/ContactControllerTest.java
@@ -0,0 +1,31 @@
+package me.oldboy.controller;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ContactControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @SneakyThrows
+    void shouldReturnOkWithAuthenticatedGetContactDetails() {
+        MvcResult mvcResult = mockMvc.perform(get("/contact"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        assertThat(mvcResult.getResponse().getContentAsString()).contains("Contact details from DB");
+    }
+}
