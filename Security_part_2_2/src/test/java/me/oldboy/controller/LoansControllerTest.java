e0cc5dd50055c7aa8ca7be97220a98802bcb3f30
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-05 17:36:22 +0500

    Security_part_2_2: Update LoansControllerTest.java

---
+++ Security_part_2_2/src/test/java/me/oldboy/controller/LoansControllerTest.java
@@ -0,0 +1,29 @@
+package me.oldboy.controller;
+
+import lombok.SneakyThrows;
+import me.oldboy.controllers.LoansController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(LoansController.class)
+@WithMockUser
+class LoansControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @SneakyThrows
+    void shouldReturn200_WithAuthGetLoanDetailsTest() {
+        mockMvc.perform(get("/myLoans"))
+               .andExpect(status().isOk())
+               .andExpect(content().string("Here are the loan details from the DB"));
+    }
+}
