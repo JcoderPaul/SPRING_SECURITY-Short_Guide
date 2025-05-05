7590b92f067c2fbd096be28fdaef69eba4cfea9d
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-05 14:54:39 +0500

    Security_part_2_1: Add ContactControllerTest.java

---
+++ Security_part_2_1/src/test/java/me/oldboy/controller/ContactControllerTest.java
@@ -0,0 +1,40 @@
+package me.oldboy.controller;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ContactControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @SneakyThrows
+    @WithMockUser
+    void shouldReturnOkWithAuthenticatedGetContactDetails() {
+        MvcResult mvcResult = mockMvc.perform(get("/contact"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        assertThat(mvcResult.getResponse().getContentAsString()).contains("Details get from DB");
+    }
+
+    @Test
+    @SneakyThrows
+    public void accessSecuredResourceUnauthenticatedThenRedirectsToLogin() {
+        mockMvc.perform(get("/contact"))
+                .andExpect(status().is4xxClientError());
+    }
+}
