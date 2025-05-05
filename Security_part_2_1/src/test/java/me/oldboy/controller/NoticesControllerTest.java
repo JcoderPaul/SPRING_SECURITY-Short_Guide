82576ccf76aaef3f6ffabf638f6c19425f55a797
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-05 17:05:55 +0500

    Security_part_2_1: Add NoticesControllerTest.java

---
+++ Security_part_2_1/src/test/java/me/oldboy/controller/NoticesControllerTest.java
@@ -0,0 +1,31 @@
+package me.oldboy.controller;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class NoticesControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    /* Явно пробросим исключение */
+    @Test
+    void shouldReturnOkWithoutAuthGetNotices() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(get("/notices"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        assertThat(mvcResult.getResponse().getContentAsString()).contains("Here are the notices details from the DB");
+    }
+}
