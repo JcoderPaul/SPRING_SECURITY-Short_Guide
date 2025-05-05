c685a69d33b204b590fb267f3f624da7c971ccc4
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-05 06:33:21 +0500

    Security_part_3_3: Add AppSecurityConfig.java

---
+++ Security_part_3_3/src/main/java/me/oldboy/config/AppSecurityConfig.java
@@ -0,0 +1,86 @@
+package me.oldboy.config;
+
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@ComponentScan("me.oldboy")
+public class AppSecurityConfig {
+
+	@Autowired
+	private DataSource dataSource;
+
+	/**
+	 * Controller endpoint:
+	 * /myAccount - Secured
+	 * /myBalance - Secured
+	 * /myLoans - Secured
+	 * /myCards - Secured
+	 * /notices - Not Secured
+	 * /contact - Not Secured
+	 */
+
+	@Bean
+	@SneakyThrows
+	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) {
+		httpSecurity.csrf(AbstractHttpConfigurer::disable)
+				.cors(AbstractHttpConfigurer::disable)
+				.authorizeHttpRequests(config ->
+						config.requestMatchers("/notices",
+										                "/contact")
+								.permitAll()
+								.requestMatchers("/myAccount",
+														  "/myBalance",
+														  "/myLoans",
+														  "/myCards")
+								.authenticated()
+								.requestMatchers("/roleList")
+								.hasRole("HR")
+								.anyRequest()
+								.authenticated())
+				.httpBasic(Customizer.withDefaults())
+				.formLogin(Customizer.withDefaults());
+
+		return httpSecurity.build();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(UserDetailsService userDetailsService,
+													   PasswordEncoder passwordEncoder) {
+		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+		authenticationProvider.setUserDetailsService(userDetailsService);
+		authenticationProvider.setPasswordEncoder(passwordEncoder);
+
+		return new ProviderManager(authenticationProvider);
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new JdbcUserDetailsManager(dataSource);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+}
