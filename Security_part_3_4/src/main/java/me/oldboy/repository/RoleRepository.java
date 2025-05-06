6653c9b843227035449dca51a573ad880fa9de11
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-06 07:03:48 +0500

    Security_part_3_4: Add RoleRepository.java

---
+++ Security_part_3_4/src/main/java/me/oldboy/repository/RoleRepository.java
@@ -0,0 +1,28 @@
+package me.oldboy.repository;
+
+import lombok.extern.slf4j.Slf4j;
+import me.oldboy.models.PrintAuthorities;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Slf4j
+@Repository
+public class RoleRepository {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    private static final String FIND_ALL_SQL = "SELECT * FROM authorities";
+
+    public List<PrintAuthorities> findAll() {
+        return jdbcTemplate.query(FIND_ALL_SQL, roleRowMapper);
+    }
+
+    private RowMapper<PrintAuthorities> roleRowMapper = (row, rowNumber) ->
+            new PrintAuthorities(row.getString("username"),
+                                 row.getString("authority"));
+}
