c61f86da122843398fb7c37de4bab706593fe3ae
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-06 06:59:13 +0500

    Security_part_3_4: Add TestBaseScripts.sql

---
+++ Security_part_3_4/SQL_SCRIPTS/TestBaseScripts.sql
@@ -0,0 +1,28 @@
+CREATE TABLE users (
+  id BIGSERIAL PRIMARY KEY ,
+  username varchar(65) unique,
+  password varchar(100),
+  enabled int
+) ;
+
+CREATE TABLE authorities (
+  id BIGSERIAL PRIMARY KEY ,
+  username varchar(65),
+  authority varchar(25),
+  FOREIGN KEY (username) references users(username)
+) ;
+
+INSERT INTO users (username, password, enabled)
+VALUES
+	('Paul', '{noop}12345', 1),
+	('Sasha', '{noop}54321', 1),
+	('Stasya', '{noop}98765', 1);
+    
+INSERT INTO authorities (username, authority)
+VALUES
+	('Paul', 'ROLE_EMPLOYEE'),
+	('Sasha', 'ROLE_HR'),
+    ('Stasya', 'ROLE_HR'),
+	('Stasya', 'ROLE_MANAGER');
+    
+    
