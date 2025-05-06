32d285fd3a4ffc27f22c42ac0dfe0ef1a42aedec
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-06 11:50:06 +0500

    Security_part_3_2: Add AccountControllerTest.java

---
+++ Security_part_3_2/src/test/java/me/oldboy/controller/AccountControllerTest.java
@@ -0,0 +1,31 @@
+package me.oldboy.controller;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AccountControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @SneakyThrows
+    @WithMockUser
+    void shouldReturnOkGetAccountDetailsTest() {
+        mockMvc.perform(MockMvcRequestBuilders.get("/myAccount"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Here are the account details from the DB"));
+    }
+}
