263160e9e3fc790ccd9aa7dfcadb8f0f25fea92b
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-07 11:30:24 +0500

    Security_part_3_3: Add AccountControllerTest.java

---
+++ Security_part_3_3/src/test/java/me/oldboy/controllers/AccountControllerTest.java
@@ -0,0 +1,61 @@
+package me.oldboy.controllers;
+
+import lombok.SneakyThrows;
+import me.oldboy.config.AppSecurityConfig;
+import me.oldboy.config.AppSecurityInitializer;
+import me.oldboy.config.AppWebInitializer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/*
+Поскольку у нас nonBoot Spring приложение, то нам понадобиться чуть больше аннотаций для настройки работоспособности тестов:
+- @ExtendWith - запускает тест в контексте Spring, без нее мы можем поймать исключение - "WebApplicationContext is required".
+- @ContextConfiguration - аннотация, которую применяют к тестовому классу для настройки метаданных, используемых для определения
+способа загрузки и настройки контекста приложения, например, в данном случае, при отсутствии данной аннотации и каких либо еще
+настроек, тест "не поймет", как соотносить ссылки endpoint-ов и методы реализующие их (No endpoint GET /myAccount).
+- @WebAppConfiguration - аннотация, которую применяют к тестовому классу, чтобы объявить, что ApplicationContext, загруженный для
+тестов, должен быть WebApplicationContext, в нашем случае мы тестируем методы контроллера, без данной аннотации "тесты не поймут"
+с каким контекстом им взаимодействовать (для тестирования нужен хоть какой-то контекст приложения).
+*/
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {AppWebInitializer.class,
+                                 AppSecurityInitializer.class,
+                                 AppSecurityConfig.class})
+@WebAppConfiguration
+class AccountControllerTest {
+
+    @Autowired
+    private WebApplicationContext webAppContext;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext)
+                                 .apply(springSecurity())    // Включаем Security для MockMvc
+                                 .build();
+    }
+
+    @Test
+    @SneakyThrows
+    @WithMockUser
+    void shouldReturnOkGetAccountDetailsTest() {
+        mockMvc.perform(MockMvcRequestBuilders.get("/myAccount"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Here are the account details from the DB"));
+    }
+}
