0359a6fb1413d3f7e9ad106269cd49269843cbb3
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-08 07:14:10 +0500

    Security_part_4_2: Add AppWebInitializer.java

---
+++ Security_part_4_2/src/main/java/me/oldboy/config/AppWebInitializer.java
@@ -0,0 +1,29 @@
+package me.oldboy.config;
+
+import lombok.extern.slf4j.Slf4j;
+import me.oldboy.config.security_config.AppSecurityConfig;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+@Slf4j
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "me.oldboy")
+public class AppWebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{AppSecurityConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{AppWebInitializer.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
