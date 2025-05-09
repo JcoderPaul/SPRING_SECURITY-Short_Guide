03fe202b25f01d741f13d1b1d9bcab42c6c1971b
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-09 21:19:57 +0500

    Security_part_5_1: add ContactController.java

---
+++ Security_part_5_1/src/main/java/me/oldboy/controllers/ContactController.java
@@ -0,0 +1,16 @@
+package me.oldboy.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class ContactController {
+	
+	@GetMapping("/contact")
+	public String saveContactInquiryDetails(String input) {
+		return "Inquiry details are saved to the DB";
+	}
+
+}
