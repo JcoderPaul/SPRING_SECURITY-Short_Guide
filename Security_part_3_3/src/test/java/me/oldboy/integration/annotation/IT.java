8096811922623c6e6615a7d9359890ed132e0131
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-10 17:21:41 +0500

    Security_part_3_3: Add IT.java

---
+++ Security_part_3_3/src/test/java/me/oldboy/integration/annotation/IT.java
@@ -0,0 +1,20 @@
+package me.oldboy.integration.annotation;
+
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@ActiveProfiles("test")
+@Sql({"classpath:sql_scripts/data.sql"})    // Создадим и заполним таблицы БД
+@WebAppConfiguration
+@Transactional
+public @interface IT {
+}
