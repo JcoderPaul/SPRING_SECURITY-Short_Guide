a31e7ae2068ce261055a572dc83ef380f4985591
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-10 07:17:36 +0500

    Security_part_5_2: Add DetailsCreateDto.java

---
+++ Security_part_5_2/src/main/java/me/oldboy/dto/details_dto/DetailsCreateDto.java
@@ -0,0 +1,15 @@
+package me.oldboy.dto.details_dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+public record DetailsCreateDto(@NotBlank
+                               @Size(min = 2, max = 64, message = "Field size value can be between 2 and 64")
+                               String clientName,
+                               @NotBlank
+                               @Size(min = 2, max = 64, message = "Field size value can be between 2 and 64")
+                               String clientSurName,
+                               @Positive(message = "Field can't be lass then 0, unless you come from a counter-directional universe!")
+                               Integer age) {
+}
