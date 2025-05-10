21be3a6c970eba03e5e94c7fb9b40acad9cccd54
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-10 17:19:36 +0500

    Security_part_3_3: Add TestWebInitializer.java

---
+++ Security_part_3_3/src/test/java/me/oldboy/config/test_context/TestWebInitializer.java
@@ -0,0 +1,30 @@
+package me.oldboy.config.test_context;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+@Slf4j
+@Configuration
+@EnableWebMvc
+@EnableAspectJAutoProxy
+@ComponentScan(basePackages = "me.oldboy.config.test_context")
+public class TestWebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
