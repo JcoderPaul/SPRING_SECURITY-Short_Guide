56d4a8522c3159a870fa23e609be39ebcb655074
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-11 08:19:55 +0500

    Security_part_6_1: Add NoticesController.java

---
+++ Security_part_6_1/src/main/java/me/oldboy/controllers/NoticesController.java
@@ -0,0 +1,21 @@
+package me.oldboy.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@Slf4j
+@RestController
+public class NoticesController {
+	
+	@GetMapping(value = "/notices", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> getNotices() throws IOException {
+		log.info("REST request health check");
+		return new ResponseEntity<>("Here are the notices details from the DB", HttpStatus.OK);
+	}
+}
