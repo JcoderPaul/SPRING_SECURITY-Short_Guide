0c3e3dea64b24b7cf8091fa20af908e3919227ba
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-12 07:28:46 +0500

    Security_part_7_1: Add ClientDetailsServiceException.java

---
+++ Security_part_7_1/src/main/java/me/oldboy/exception/handlers/ClientDetailsServiceException.java
@@ -0,0 +1,19 @@
+package me.oldboy.exception.handlers;
+
+import lombok.extern.slf4j.Slf4j;
+import me.oldboy.exception.exception_entity.ExceptionResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Slf4j
+@ControllerAdvice
+public class ClientDetailsServiceException {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ExceptionResponse> handleExceptions(RuntimeException exception) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(exception.getMessage());
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+}
