c8e2f104008620f3070acb0773d084d08427840f
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-12 07:24:20 +0500

    Security_part_7_1: Add SecurityDemoAppPart_7_1.java

---
+++ Security_part_7_1/src/main/java/me/oldboy/SecurityDemoAppPart_7_1.java
@@ -0,0 +1,15 @@
+package me.oldboy;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@Slf4j
+@SpringBootApplication
+@ComponentScan("me.oldboy")
+public class SecurityDemoAppPart_7_1 {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityDemoAppPart_7_1.class, args);
+    }
+}
