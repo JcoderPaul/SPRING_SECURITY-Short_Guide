b28e951b5e445dc1c50152349ed9c11733096657
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-12 07:30:27 +0500

    Security_part_7_1: Add ClientService.java

---
+++ Security_part_7_1/src/main/java/me/oldboy/services/ClientService.java
@@ -0,0 +1,53 @@
+package me.oldboy.services;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import me.oldboy.dto.client_dto.ClientCreateDto;
+import me.oldboy.dto.client_dto.ClientReadDto;
+import me.oldboy.mapper.ClientMapper;
+import me.oldboy.models.Client;
+import me.oldboy.models.Role;
+import me.oldboy.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@NoArgsConstructor
+@AllArgsConstructor
+@Transactional(readOnly = true)
+public class ClientService {
+
+    @Autowired
+    private ClientRepository clientRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public ClientReadDto saveClient(ClientCreateDto clientCreateDto){
+        String encodedPass = passwordEncoder.encode(clientCreateDto.getPass());
+
+        Client toCreateClient = ClientMapper.INSTANCE.mapToClient(clientCreateDto);
+        toCreateClient.setRole(Role.USER);
+        toCreateClient.setPass(encodedPass);
+        Client createdClient = clientRepository.save(toCreateClient);
+        ClientReadDto toPrintClient = ClientMapper.INSTANCE.mapToClientReadDto(createdClient);
+
+        return toPrintClient;
+    }
+
+    public List<ClientReadDto> findAll(){
+        return clientRepository.findAll().stream()
+                                         .map(client -> ClientMapper.INSTANCE.mapToClientReadDto(client))
+                                         .collect(Collectors.toList());
+    }
+
+    public Optional<Client> findByEmail(String email){
+        return clientRepository.findByEmail(email);
+    }
+ }
