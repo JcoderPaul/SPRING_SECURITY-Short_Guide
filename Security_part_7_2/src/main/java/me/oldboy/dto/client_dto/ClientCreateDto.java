b2aaf4d820fe39088dae3ac0c47ba366fe840339
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-13 06:47:23 +0500

    Security_part_7_2: add ClientCreateDto.java

---
+++ Security_part_7_2/src/main/java/me/oldboy/dto/client_dto/ClientCreateDto.java
@@ -0,0 +1,24 @@
+package me.oldboy.dto.client_dto;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import me.oldboy.dto.details_dto.DetailsCreateDto;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class ClientCreateDto{
+        @Email(message = "Standard e-mail structure - email_name@email_domain.top_lavel_domain (for example: paul@tradsystem.ru)")
+        String email;
+        @NotBlank(message = "Password cannot be blank")
+        @Size(min = 2, max = 64, message = "Password size can be between 2 and 64")
+        String pass;
+        /* @CheckDetails */
+        @Valid
+        DetailsCreateDto details;
+}
