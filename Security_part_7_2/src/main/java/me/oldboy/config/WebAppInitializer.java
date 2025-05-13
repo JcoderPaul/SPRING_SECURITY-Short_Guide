5b98dd9a73251a5c98709194fd849433e3483f8f
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-13 06:45:20 +0500

    Security_part_7_2: add WebAppInitializer.java

---
+++ Security_part_7_2/src/main/java/me/oldboy/config/WebAppInitializer.java
@@ -0,0 +1,23 @@
+package me.oldboy.config;
+
+import lombok.extern.slf4j.Slf4j;
+import me.oldboy.config.security_config.AppSecurityConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+@Slf4j
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{AppSecurityConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{ViewResolversConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
