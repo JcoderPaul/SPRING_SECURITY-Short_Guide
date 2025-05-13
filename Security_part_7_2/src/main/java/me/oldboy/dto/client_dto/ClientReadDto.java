e75b3ea0275300b6d4b7cdd07c0da4b8ba908a48
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-13 06:47:34 +0500

    Security_part_7_2: add ClientReadDto.java

---
+++ Security_part_7_2/src/main/java/me/oldboy/dto/client_dto/ClientReadDto.java
@@ -0,0 +1,16 @@
+package me.oldboy.dto.client_dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class ClientReadDto {
+    private String email;
+    private String role;
+    private String clientName;
+    private String clientSurName;
+    private Integer age;
+}
