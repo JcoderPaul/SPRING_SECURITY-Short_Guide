48a1c4b989a3ab5e104c183059c64ee58317785b
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-13 06:48:00 +0500

    Security_part_7_2: add ExceptionResponse.java

---
+++ Security_part_7_2/src/main/java/me/oldboy/exception/exception_entity/ExceptionResponse.java
@@ -0,0 +1,14 @@
+package me.oldboy.exception.exception_entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ExceptionResponse {
+    private String exceptionMsg;
+}
