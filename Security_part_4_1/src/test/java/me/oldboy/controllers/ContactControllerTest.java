40081763e173fd572f6112c0b722ccf1a7e8bb64
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-14 07:21:40 +0500

    Security_part_4_1: Add ContactControllerTest.java

---
+++ Security_part_4_1/src/test/java/me/oldboy/controllers/ContactControllerTest.java
@@ -0,0 +1,39 @@
+package me.oldboy.controllers;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@SpringBootTest
+class ContactControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @BeforeEach
+    void setContext(){
+        mockMvc = webAppContextSetup(webApplicationContext)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    @SneakyThrows
+    void shouldReturnOkForEveryOneClientGetContactDetailsTest() {
+        mockMvc.perform(get("/contact"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Get details from DB"));
+    }
+}
