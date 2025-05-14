7b8419967b3e19a71615ee803f528e1970649bf5
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-14 19:49:34 +0500

    Security_part_4_1: Add TestFields.java

---
+++ Security_part_4_1/src/test/java/me/oldboy/test_content/TestFields.java
@@ -0,0 +1,11 @@
+package me.oldboy.test_content;
+
+public class TestFields {
+    public final static String TEST_EMAIL = "for_test@test.com";
+    public final static String TEST_PASS = "1234567";
+    public final static String TEST_CLIENT_NAME = "Testino";
+    public final static String TEST_CLIENT_SUR_NAME = "Testorelly";
+    public final static Integer TEST_AGE = 32;
+
+    public final static String EXIST_EMAIL = "admin@test.com";
+}
