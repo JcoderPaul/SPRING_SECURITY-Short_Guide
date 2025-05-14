5ebc8cd17f6427d2c1935c9b52d9807366f36f91
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-14 11:13:05 +0500

    Security_part_4_1: Add ClientRepository.java

--- Security_part_4_1/src/main/java/me/oldboy/repository/ClientRepository.java
+++ Security_part_4_1/src/main/java/me/oldboy/repository/ClientRepository.java
@@ -2,11 +2,13 @@ package me.oldboy.repository;
 
 import me.oldboy.models.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface ClientRepository extends JpaRepository<Client, Long> {
+public interface ClientRepository extends JpaRepository<Client, Long>,
+                                          CrudRepository<Client, Long> {
     Optional<Client> findByEmail(String email);
 }
