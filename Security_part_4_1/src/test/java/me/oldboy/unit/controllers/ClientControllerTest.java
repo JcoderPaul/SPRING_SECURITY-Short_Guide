3da4132e438e0efb37582765247e4c5237dc3939
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-15 16:16:57 +0500

    Security_part_4_1: Update ClientControllerTest.java

--- Security_part_4_1/src/test/java/me/oldboy/unit/controllers/ClientControllerTest.java
+++ Security_part_4_1/src/test/java/me/oldboy/unit/controllers/ClientControllerTest.java
@@ -190,11 +190,11 @@ class ClientControllerTest {
         /* Формируем массив потенциально известных ошибок */
         List<ObjectError> errors = Arrays.asList(
                 new FieldError("clientCreateDto",
-                                    "pass",
-                                    "Field size value can be between 2 and 64"),
+                        "pass",
+                        "Field size value can be between 2 and 64"),
                 new FieldError("clientCreateDto",
-                                    "email",
-                                    "Standard e-mail structure - email_name@email_domain.top_lavel_domain (for example: paul@tradsystem.ru)")
+                        "email",
+                        "Standard e-mail structure - email_name@email_domain.top_lavel_domain (for example: paul@tradsystem.ru)")
         );
 
         /* Mock-аем работу методов BindingResult */
