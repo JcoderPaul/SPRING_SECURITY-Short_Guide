6ba59103e60da7f28e0f6b92cae436c62c1cd31e
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-16 14:08:54 +0500

    Security_part_4_2: update DetailsCreateDto.java

--- Security_part_4_2/src/main/java/me/oldboy/dto/details_dto/DetailsCreateDto.java
+++ Security_part_4_2/src/main/java/me/oldboy/dto/details_dto/DetailsCreateDto.java
@@ -1,6 +1,7 @@
 package me.oldboy.dto.details_dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
@@ -10,6 +11,7 @@ public record DetailsCreateDto(@NotBlank
                                @NotBlank
                                @Size(min = 2, max = 64, message = "Field size value can be between 2 and 64")
                                String clientSurName,
+                               @NotNull
                                @Positive(message = "Field can't be lass then 0, unless you come from a counter-directional universe!")
                                Integer age) {
 }
