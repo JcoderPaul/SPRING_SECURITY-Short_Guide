6e62398b9312420109fae6990ce37c5012caecc8
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-18 17:49:48 +0500

    Security_part_4_1: Update ClientControllerTest.java

--- Security_part_4_1/src/test/java/me/oldboy/unit/controllers/ClientControllerTest.java
+++ Security_part_4_1/src/test/java/me/oldboy/unit/controllers/ClientControllerTest.java
@@ -20,7 +20,6 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
