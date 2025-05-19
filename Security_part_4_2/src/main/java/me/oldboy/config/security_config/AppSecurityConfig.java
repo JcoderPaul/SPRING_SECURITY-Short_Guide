9e48c19c7994f884e8186618229852d284709574
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-19 07:36:52 +0500

    Security_part_4_2: Update AppSecurityConfig.java

--- Security_part_4_2/src/main/java/me/oldboy/config/security_config/AppSecurityConfig.java
+++ Security_part_4_2/src/main/java/me/oldboy/config/security_config/AppSecurityConfig.java
@@ -2,26 +2,26 @@ package me.oldboy.config.security_config;
 
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import me.oldboy.config.securiry_details.ClientDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Slf4j
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
-@ComponentScan("me.oldboy")
+@ComponentScan({
+	"me.oldboy.controllers"
+	,"me.oldboy.services"
+	,"me.oldboy.repository"
+	,"me.oldboy.config.data_source"
+	,"me.oldboy.config.security_details"
+	,"me.oldboy.config.auth_config"
+})
 public class AppSecurityConfig {
 
 	/*
@@ -32,27 +32,10 @@ public class AppSecurityConfig {
 	нужно помнить, что любое Spring Web приложение будь то Boot или nonBoot нуждается в контейнере
 	сервлетов, который тоже выполняет некую работу будь он внешний или интегрированный.
 	*/
-	@Autowired
-	private ClientDetailsService clientDetailsService;
 
 	@Bean
 	@SneakyThrows
 	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) {
 		return FilterChainConfig.getSecurityFilterChain(httpSecurity);
 	}
-
-	@Bean
-	public AuthenticationManager authenticationManager(PasswordEncoder passwordEncoder) {
-		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-		authenticationProvider.setUserDetailsService(clientDetailsService);
-		authenticationProvider.setPasswordEncoder(passwordEncoder);
-
-		return new ProviderManager(authenticationProvider);
-	}
-
-	@Bean
-	public PasswordEncoder getPasswordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
 }
