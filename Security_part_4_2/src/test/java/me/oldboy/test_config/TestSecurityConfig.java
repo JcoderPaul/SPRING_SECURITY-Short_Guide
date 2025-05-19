aef9431af583180c97f1d489b3548d9223200a75
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-19 07:44:17 +0500

    Security_part_4_2: Add TestSecurityConfig.java

---
+++ Security_part_4_2/src/test/java/me/oldboy/test_config/TestSecurityConfig.java
@@ -0,0 +1,26 @@
+package me.oldboy.test_config;
+
+import me.oldboy.config.security_config.FilterChainConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@ComponentScan({
+    "me.oldboy.controllers"
+    ,"me.oldboy.services"
+    ,"me.oldboy.repository"
+    ,"me.oldboy.config.security_details"
+    ,"me.oldboy.repository"
+    ,"me.oldboy.config.auth_config"
+})
+public class TestSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return FilterChainConfig.getSecurityFilterChain(httpSecurity);
+    }
+}
