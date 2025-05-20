a61da9bd508638c5fd4be56b74d26af2248d7471
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-20 06:18:56 +0500

    Security_part_5_1: add ContactControllerTest.java

---
+++ Security_part_5_1/src/test/java/me/oldboy/controllers/ContactControllerTest.java
@@ -0,0 +1,40 @@
+package me.oldboy.controllers;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@SpringBootTest // Загружает контекст Spring (как при запуске приложения).
+class ContactControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @BeforeEach
+    void setContext(){
+        mockMvc = webAppContextSetup(webApplicationContext).apply(springSecurity()).build();
+    }
+
+    @Test
+    @SneakyThrows
+    void shouldReturnOkForEveryOneClientGetContactDetailsTest() {
+        MvcResult mvcResult = mockMvc.perform(get("/contact"))
+                                     .andExpect(status().isOk())
+                                     .andReturn();
+
+        assertThat(mvcResult.getResponse().getContentAsString().contains("Get contact from DB"));
+    }
+}
