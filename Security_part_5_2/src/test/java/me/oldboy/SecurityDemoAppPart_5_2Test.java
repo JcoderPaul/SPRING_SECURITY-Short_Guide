e6eb01214d2613bd27b53f071ceb3084870ff26e
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-21 14:07:58 +0500

    Security_part_5_2: add SecurityDemoAppPart_5_2Test.java

---
+++ Security_part_5_2/src/test/java/me/oldboy/SecurityDemoAppPart_5_2Test.java
@@ -0,0 +1,19 @@
+package me.oldboy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class SecurityDemoAppPart_5_2Test {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Test
+    void mainContextLoaderTest(){
+        assertNotNull(webApplicationContext);
+    }
+}
