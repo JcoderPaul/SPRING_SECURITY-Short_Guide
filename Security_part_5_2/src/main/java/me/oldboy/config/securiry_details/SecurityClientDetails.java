997b7bb62021d8b42e249993172b249dbdad8fec
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-21 14:19:42 +0500

    Security_part_5_2: update SecurityClientDetails.java

--- Security_part_5_2/src/main/java/me/oldboy/config/securiry_details/SecurityClientDetails.java
+++ Security_part_5_2/src/main/java/me/oldboy/config/securiry_details/SecurityClientDetails.java
@@ -1,6 +1,7 @@
 package me.oldboy.config.securiry_details;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 import me.oldboy.models.Client;
 import org.springframework.security.core.GrantedAuthority;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class SecurityClientDetails implements UserDetails {
 
 	private Client client;
