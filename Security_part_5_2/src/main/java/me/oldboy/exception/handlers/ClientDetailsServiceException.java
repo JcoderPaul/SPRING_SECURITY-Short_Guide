d45c73d1161b7f9fa1a2be2f895bbf1969212fe1
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-21 14:03:20 +0500

    Security_part_5_2: update ClientDetailsServiceException.java

--- Security_part_5_2/src/main/java/me/oldboy/exception/handlers/ClientDetailsServiceException.java
+++ Security_part_5_2/src/main/java/me/oldboy/exception/handlers/ClientDetailsServiceException.java
@@ -1,9 +1,13 @@
 package me.oldboy.exception.handlers;
 
 import lombok.extern.slf4j.Slf4j;
+import me.oldboy.exception.DuplicateClientEmailException;
+import me.oldboy.exception.EmailNotFoundException;
 import me.oldboy.exception.exception_entity.ExceptionResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -11,7 +15,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ClientDetailsServiceException {
 
-    @ExceptionHandler(RuntimeException.class)
+    @ExceptionHandler({UsernameNotFoundException.class,
+                       EmailNotFoundException.class,
+                       AuthenticationException.class,
+                       DuplicateClientEmailException.class
+    })
     public ResponseEntity<ExceptionResponse> handleExceptions(RuntimeException exception) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(exception.getMessage());
         return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
