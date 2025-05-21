19cb94a2019ff2516364f28ebad562b3c8919ea9
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-21 10:12:59 +0500

    Security_part_5_1: Update ClientControllerTest.java

--- Security_part_5_1/src/test/java/me/oldboy/unit/controllers/ClientControllerTest.java
+++ Security_part_5_1/src/test/java/me/oldboy/unit/controllers/ClientControllerTest.java
@@ -22,7 +22,6 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -49,8 +48,6 @@ class ClientControllerTest {
     private BindingResult mockBindingResult;
 
     /* Для тестирования метода getAdminName() */
-    @Mock
-    private SecurityContext mockSecurityContext;
     @Mock
     private Authentication mockAuthentication;
     @Mock
