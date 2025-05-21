cf6b4aaa7112e55f207d9ae7b570cccd0fc3171e
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-21 10:20:05 +0500

    Security_part_5_1: Update TestConstantFields.java

--- Security_part_5_1/src/test/java/me/oldboy/test_content/TestConstantFields.java
+++ Security_part_5_1/src/test/java/me/oldboy/test_content/TestConstantFields.java
@@ -6,6 +6,8 @@ public class TestConstantFields {
     public final static String TEST_CLIENT_NAME = "Testino";
     public final static String TEST_CLIENT_SUR_NAME = "Testorelly";
     public final static Integer TEST_AGE = 32;
+    public final static String TEST_STR_ROLE_USER = "USER";
+    public final static String TEST_STR_ROLE_ADMIN = "ADMIN";
 
     public final static String EXIST_EMAIL = "admin@test.com";
     public final static String NON_EXIST_EMAIL = "non_email@test.com";
