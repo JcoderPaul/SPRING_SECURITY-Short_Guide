93ef0a21fa6dbbc094de707eef50382ad0fdef16
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-23 18:21:14 +0500

    Security_part_6_1: add TestContainerInit.java

---
+++ Security_part_6_1/src/test/java/me/oldboy/integration/TestContainerInit.java
@@ -0,0 +1,37 @@
+package me.oldboy.integration;
+
+import me.oldboy.integration.annotation.IT;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@IT
+@Sql({"classpath:sql_scripts/data.sql"})
+public abstract class TestContainerInit {
+   /*
+       Получаем тест контейнер, в параметрах передаем тэг нашей Docker БД. Обычно, значений по-умолчанию
+       самого PostgreSQLContainer хватает, но их легко можно задать используя его же методы.
+   */
+    private static final PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:13");
+
+    /*
+        Запускаем тест контейнер. Хотя аннотация говорит о запуске контейнера перед каждым тестом, но, если
+        зайти в метод *.start(), то можно заметить, как ID контейнера определяется единожды для всех тестов.
+    */
+    @BeforeAll
+    static void runContainer() {
+        container.start();
+    }
+
+    /* Получаем свойства сгенерированные динамически при запуске тестов, нам нужен был URL, вот его мы и извлекаем. */
+    @DynamicPropertySource
+    static void postgresProperties(DynamicPropertyRegistry registry) {
+        /*
+            DynamicPropertyRegistry - реестр, используемый с методами @DynamicPropertySource,
+            чтобы они могли добавлять в среду свойства, имеющие динамически разрешаемые значения.
+        */
+        registry.add("spring.datasource.url", () -> container.getJdbcUrl());
+    }
+}
