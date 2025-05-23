6c8d99feccded06615453e2cd6c68aedb97768c8
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-23 20:06:44 +0500

    Security_part_6_1: add db.changelog-5.0.sql

---
+++ Security_part_6_1/src/main/resources/db/changelog/db.changelog-5.0.sql
@@ -0,0 +1,10 @@
+--liquibase formatted sql
+
+--changeset oldboy:1
+INSERT INTO clients (id, email, pass, role, details_id)
+VALUES (1, 'admin@test.com', '$2y$10$OZZhrg6W3oekdIgsLjtJJO.NmgTVBV6jGNWnN9UVD2OnbXvX8.4I2', 'ADMIN', 1),
+       (2, 'admin2@test.com', '$2y$10$Y7/d1fA/f6v0B1O5bYZzvutMgoYThaqCvg.J9O3DdwYRU70EP..WO', 'ADMIN', 2),
+       (3, 'user@test.com', '$2y$10$U1kgGBVqHFJ4AcnawvUdmeBxRE3593XZ4U0lEJCV2RWI/J3DW/f.G', 'USER', 3),
+       (4, 'user2@test.com', '$2y$10$H1TwhepYL0AyVUb3f3OCQupyDBiTL7DcWCgLfTqzFDXZl5JpsaU7a', 'USER', 4),
+       (5, 'user3@test.com', '$2y$10$YIKMHeHT0kCqB.PZHNDTF..PG3cFeymimZ48wGkxFeFMWmmE5dW46', 'USER', 5);
+--rollback delete from clients;
