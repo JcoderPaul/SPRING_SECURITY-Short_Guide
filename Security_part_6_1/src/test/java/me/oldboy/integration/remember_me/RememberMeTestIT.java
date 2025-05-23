c1b0f2a370d08aeee61e52606a8e177a6491e53e
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-23 22:21:21 +0500

    Security_part_6_1: add RememberMeTestIT.java

---
+++ Security_part_6_1/src/test/java/me/oldboy/integration/remember_me/RememberMeTestIT.java
@@ -0,0 +1,40 @@
+package me.oldboy.integration.remember_me;
+
+import jakarta.servlet.http.Cookie;
+import lombok.SneakyThrows;
+import me.oldboy.integration.TestContainerInit;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static me.oldboy.test_constant.TestConstantFields.EXIST_EMAIL;
+import static me.oldboy.test_constant.TestConstantFields.TEST_PASS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+class RememberMeTestIT extends TestContainerInit {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @SneakyThrows
+    void shouldReturn_3xx_And_RememberMeCookie_Test() {
+        MvcResult mvcResult = mockMvc.perform(post("/login")
+                        .param("username", EXIST_EMAIL)
+                        .param("password", TEST_PASS)
+                        .param("remember-me", "true"))
+                .andExpect(status().is3xxRedirection())
+                .andReturn();
+
+        Cookie rememberMeCookie = mvcResult.getResponse().getCookie("remember-me");
+
+        assertThat(rememberMeCookie).isNotNull();
+        assertThat(rememberMeCookie.getMaxAge() > 0).isTrue();
+        assertThat(rememberMeCookie.getValue().isEmpty()).isFalse();
+    }
+}
