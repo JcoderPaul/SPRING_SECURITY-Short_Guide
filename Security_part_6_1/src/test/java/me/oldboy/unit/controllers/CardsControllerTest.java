ec178c18cfdf785cb965a34a0c6b27d9d05bdc41
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-23 16:07:12 +0500

    Security_part_6_1: add CardsControllerTest.java

---
+++ Security_part_6_1/src/test/java/me/oldboy/unit/controllers/CardsControllerTest.java
@@ -0,0 +1,45 @@
+package me.oldboy.unit.controllers;
+
+import lombok.SneakyThrows;
+import me.oldboy.controllers.CardsController;
+import me.oldboy.repository.ClientRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.sql.DataSource;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(CardsController.class)
+class CardsControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private DataSource dataSource;
+    @MockBean
+    private ClientRepository clientRepository;
+
+    @Test
+    @SneakyThrows
+    @WithMockUser
+    void shouldReturn_2XX_WithAuthUser_GetCardDetailsTest() {
+        mockMvc.perform(get("/myCards"))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(content().string("Here are the card details from the DB"));
+    }
+
+    @Test
+    @SneakyThrows
+    void shouldReturn_4XX_WithoutAuthUser_GetCardDetailsTest() {
+        mockMvc.perform(get("/myCards"))
+                .andExpect(status().is4xxClientError())
+                .andExpect(content().string(""));
+    }
+}
