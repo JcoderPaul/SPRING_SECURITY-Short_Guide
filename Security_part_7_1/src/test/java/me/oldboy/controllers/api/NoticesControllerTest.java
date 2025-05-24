3130aec770e78c5b5a5dbe8304b1538d92a5ef0d
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-24 23:22:00 +0500

    Security_part_7_1: add NoticesControllerTest.java

---
+++ Security_part_7_1/src/test/java/me/oldboy/controllers/api/NoticesControllerTest.java
@@ -0,0 +1,32 @@
+package me.oldboy.controllers.api;
+
+import me.oldboy.repository.ClientRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.sql.DataSource;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(NoticesController.class)
+class NoticesControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private DataSource dataSource;
+    @MockBean
+    private ClientRepository clientRepository;
+
+    @Test
+    void shouldReturn_isOk_WithAnyUser_GetNoticesTest() throws Exception {
+        mockMvc.perform(get("/notices"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("[\"Here are the notices details from the DB\"]"));
+    }
+}
