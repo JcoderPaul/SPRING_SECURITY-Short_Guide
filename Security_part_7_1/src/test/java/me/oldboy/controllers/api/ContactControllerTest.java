31fb24e71b878f6fc018eb204671382a8d69ac14
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-24 23:21:36 +0500

    Security_part_7_1: add ContactControllerTest.java

---
+++ Security_part_7_1/src/test/java/me/oldboy/controllers/api/ContactControllerTest.java
@@ -0,0 +1,34 @@
+package me.oldboy.controllers.api;
+
+import lombok.SneakyThrows;
+import me.oldboy.repository.ClientRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.sql.DataSource;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ContactController.class)
+class ContactControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private DataSource dataSource;
+    @MockBean
+    private ClientRepository clientRepository;
+
+    @Test
+    @SneakyThrows
+    void shouldReturnOkForEveryOneClientGetContactDetailsTest() {
+       mockMvc.perform(get("/contact"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Get details from DB"));
+    }
+}
