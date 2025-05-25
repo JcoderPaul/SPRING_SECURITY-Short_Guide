f5048836eb72b27d33e20fce297217062fecf4a0
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-25 07:52:20 +0500

    Security_part_9_1: add ContactService.java

---
+++ Security_part_9_1/src/main/java/me/oldboy/services/ContactService.java
@@ -0,0 +1,35 @@
+package me.oldboy.services;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import me.oldboy.dto.contact_dto.ContactReadDto;
+import me.oldboy.mapper.ContactMapper;
+import me.oldboy.models.client_info.Contact;
+import me.oldboy.repository.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@NoArgsConstructor
+@AllArgsConstructor
+@Transactional(readOnly = true)
+public class ContactService {
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    /* Работает общая аннотация над классом @Transactional, тут мы только читаем */
+    public Optional<ContactReadDto> readContact(Long clientId) {
+        Optional<ContactReadDto> contact = Optional.empty();
+        Optional<Contact> mayBeContact = contactRepository.findByClientId(clientId);
+        if (mayBeContact.isEmpty()) {
+            return contact;
+        } else {
+            contact = Optional.of(ContactMapper.INSTANCE.mapToContactReadDto(mayBeContact.get()));
+            return contact;
+        }
+    }
+}
