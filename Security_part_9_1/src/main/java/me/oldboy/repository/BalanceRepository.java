e6dcb377aab71ac0a7439391944e9abc0bf4204e
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-25 07:50:56 +0500

    Security_part_9_1: add BalanceRepository.java

---
+++ Security_part_9_1/src/main/java/me/oldboy/repository/BalanceRepository.java
@@ -0,0 +1,25 @@
+package me.oldboy.repository;
+
+import me.oldboy.models.money.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface BalanceRepository extends JpaRepository<Transaction, Long> {
+
+    @Query(value = "SELECT at.* " +
+                   "FROM account_transactions AS at " +
+                   "WHERE at.client_id = :clientId",
+            nativeQuery = true)
+    Optional<List<Transaction>> findByClientId(Long clientId);
+
+    @Query(value = "SELECT at.* " +
+                   "FROM account_transactions AS at " +
+                   "WHERE at.account_number = :accountNumber",
+            nativeQuery = true)
+    Optional<List<Transaction>> findByAccountNumber(Long accountNumber);
+}
