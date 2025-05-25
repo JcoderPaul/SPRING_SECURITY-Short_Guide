e932146f5d92b09908dda2ad2e48dd266c6c9144
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-25 07:39:37 +0500

    Security_part_9_1: add ClientDetailsService.java

---
+++ Security_part_9_1/src/main/java/me/oldboy/config/securiry_details/ClientDetailsService.java
@@ -0,0 +1,34 @@
+package me.oldboy.config.securiry_details;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import me.oldboy.exception.EmailNotFoundException;
+import me.oldboy.models.client.Client;
+import me.oldboy.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClientDetailsService implements UserDetailsService {
+
+	@Autowired
+	private ClientRepository clientRepository;
+
+	/* Извлекаем из БД клиентов (user-ов) по имени */
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<Client> mayBeClient = clientRepository.findByEmail(username);
+		if (mayBeClient.isEmpty()) {
+			throw new EmailNotFoundException("User email : " + username + " not found!");
+		}
+		return new SecurityClientDetails(mayBeClient.get());
+	}
+
+}
