606620b84d939bd0fa854fa63b60eead307868f2
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-26 14:04:30 +0500

    Security_part_9_2: add JwtTokenValidatorAndBeforeFilter.java

---
+++ Security_part_9_2/src/main/java/me/oldboy/filters/JwtTokenValidatorAndBeforeFilter.java
@@ -0,0 +1,66 @@
+package me.oldboy.filters;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import me.oldboy.config.auth_event_listener.AuthenticationEventListener;
+import me.oldboy.constants.SecurityConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.crypto.SecretKey;
+
+/* До UsernamePasswordAuthenticationFilter.class */
+@Slf4j
+@RequiredArgsConstructor
+public class JwtTokenValidatorAndBeforeFilter extends OncePerRequestFilter {
+
+	@Autowired
+	private final AuthenticationEventListener authenticationEventListener;
+
+	@Override
+	public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		log.info("-- 2 - Start JwtTokenValidator or BeforeFilter");
+
+		String jwt = request.getHeader(SecurityConstants.JWT_HEADER);
+
+		if (jwt != null) {
+			try {
+				SecretKey key = Keys.hmacShaKeyFor(SecurityConstants.JWT_KEY.getBytes(StandardCharsets.UTF_8));
+
+				Claims claims = Jwts.parser()
+						.verifyWith(key)
+						.build()
+						.parseSignedClaims(jwt)
+						.getPayload();
+
+				String principal = claims.getSubject();
+				String authorities = (String) claims.get("authorities");
+
+				Authentication auth = new UsernamePasswordAuthenticationToken(principal,null,
+						AuthorityUtils.commaSeparatedStringToAuthorityList(authorities));
+
+				SecurityContextHolder.getContext().setAuthentication(auth);
+
+			}catch (Exception e) {
+				throw new BadCredentialsException("Invalid Token received!");
+			}
+		}
+		chain.doFilter(request, response);
+		log.info("-- 2 - Finish JwtTokenValidator or BeforeFilter");
+	}
+}
