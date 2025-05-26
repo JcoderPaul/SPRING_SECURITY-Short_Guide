d68fe0892d3d65480b1f46c7929e69e3becdcc27
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-26 21:41:13 +0500

    Security_part_7_1: add AppRestSecurityConfig.java

---
+++ Security_part_7_1/src/main/java/me/oldboy/config/AppRestSecurityConfig.java
@@ -0,0 +1,51 @@
+package me.oldboy.config;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@ComponentScan("me.oldboy")
+@Order(5)
+public class AppRestSecurityConfig {
+
+    @Bean
+    @SneakyThrows
+    public SecurityFilterChain restFilterChain(HttpSecurity httpSecurity) {
+        httpSecurity
+                .securityMatcher("/api/**")
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(urlConfig ->	urlConfig.
+                        requestMatchers(antMatcher("/api/notices"),
+                                antMatcher("/api/contact"),
+                                antMatcher("/css/**"),
+                                antMatcher("/webui/login"),
+                                antMatcher("/webui/registration"))
+                        .permitAll()
+                        .requestMatchers(antMatcher("/api/myAccount"),
+                                antMatcher("/api/myBalance"),
+                                antMatcher("/api/myLoans"),
+                                antMatcher("/api/myCards"))
+                        .authenticated()
+                        .requestMatchers(antMatcher("/api/admin/**"))
+                        .hasAuthority("ADMIN")
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+}
