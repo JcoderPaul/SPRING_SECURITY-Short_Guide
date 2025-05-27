f4add46ba2dc2817db844bc0728d4f8472ad50f9
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-27 06:16:56 +0500

    Security_part_7_1: update AccountControllerTest.java

--- Security_part_7_1/src/test/java/me/oldboy/controllers/api/AccountControllerTest.java
+++ Security_part_7_1/src/test/java/me/oldboy/controllers/api/AccountControllerTest.java
@@ -14,11 +14,6 @@ import javax.sql.DataSource;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-/*
-    В этот раз в отличие от предыдущих, мы имеем в цепочке безопасности больше настроек
-    и самое важно, что при обращении к защищенному endpoint-у, без аутентификации нас
-    перебросит на страницу логина.
-*/
 @WebMvcTest(AccountController.class)
 class AccountControllerTest {
 
@@ -33,21 +28,16 @@ class AccountControllerTest {
     @SneakyThrows
     @WithMockUser // Имитируем аутентифицированного пользователя
     void shouldReturnOkWithAuthClientGetAccountDetailsTest(){
-        mockMvc.perform(get("/myAccount"))
+        mockMvc.perform(get("/api/myAccount"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("Here are the account details from the DB"));
     }
 
-    /*
-        Раньше при отсутствии аутентификации мы получали ответ из списка 4хх,
-        теперь, как описано выше, будет перенаправление запроса или 3хх ответ.
-    */
     @Test
     @SneakyThrows
     void shouldReturnUnAuthWithoutAuthClientGetAccountDetailsTest(){
-        mockMvc.perform(get("/myAccount"))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(content().string(""))
-                .andExpect(redirectedUrlPattern("**/login"));
+        mockMvc.perform(get("/api/myAccount"))
+                .andExpect(status().isUnauthorized())
+                .andExpect(content().string(""));
     }
 }
