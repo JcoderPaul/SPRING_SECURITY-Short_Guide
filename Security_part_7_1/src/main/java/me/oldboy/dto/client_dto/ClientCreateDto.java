6288e16b7e1ca721a3713e35ae87414d1d26cc31
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-27 06:16:07 +0500

    Security_part_7_1: update ClientCreateDto.java

--- Security_part_7_1/src/main/java/me/oldboy/dto/client_dto/ClientCreateDto.java
+++ Security_part_7_1/src/main/java/me/oldboy/dto/client_dto/ClientCreateDto.java
@@ -6,7 +6,9 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 import me.oldboy.dto.details_dto.DetailsCreateDto;
+import me.oldboy.validation.annitation.CheckDetails;
 
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -15,11 +17,11 @@ import me.oldboy.dto.details_dto.DetailsCreateDto;
 public class ClientCreateDto{
         @NotBlank(message = "Password cannot be blank")
         @Email(message = "Standard e-mail structure - email_name@email_domain.top_lavel_domain (for example: paul@tradsystem.ru)")
-        String email;
+        private String email;
         @NotBlank(message = "Password cannot be blank")
         @Size(min = 2, max = 64, message = "Password size can be between 2 and 64")
-        String pass;
-        /* @CheckDetails */
+        private String pass;
+        @CheckDetails
         @Valid
-        DetailsCreateDto details;
+        private DetailsCreateDto details;
 }
