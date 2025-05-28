19f77a4191938c4d13fd94b81f2e67c5ae6d4974
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-28 06:39:25 +0500

    Security_part_7_1: Add ClientServiceTestIT.java

---
+++ Security_part_7_1/src/test/java/me/oldboy/integration/services/ClientServiceTestIT.java
@@ -0,0 +1,73 @@
+package me.oldboy.integration.services;
+
+import lombok.RequiredArgsConstructor;
+import me.oldboy.dto.client_dto.ClientCreateDto;
+import me.oldboy.dto.client_dto.ClientReadDto;
+import me.oldboy.dto.details_dto.DetailsCreateDto;
+import me.oldboy.integration.TestContainerInit;
+import me.oldboy.models.Role;
+import me.oldboy.services.ClientService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static me.oldboy.test_constant.TestConstantFields.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@RequiredArgsConstructor
+class ClientServiceTestIT extends TestContainerInit {
+
+    private final ClientService clientService;
+    private ClientCreateDto testClientCreateDto;
+    private DetailsCreateDto testDetailsCreateDto;
+
+    @BeforeEach
+    void setUp(){
+        testDetailsCreateDto = new DetailsCreateDto(TEST_CLIENT_NAME, TEST_CLIENT_SUR_NAME, TEST_AGE);
+        testClientCreateDto = new ClientCreateDto(TEST_EMAIL, TEST_PASS, testDetailsCreateDto);
+    }
+
+    @Test
+    void shouldReturnClientReadDtoSaveClientTest() {
+        ClientReadDto savedClient = clientService.saveClient(testClientCreateDto);
+
+        assertAll(
+                () -> assertThat(savedClient.getClientName()).isEqualTo(testClientCreateDto.getDetails().clientName()),
+                () -> assertThat(savedClient.getClientSurName()).isEqualTo(testClientCreateDto.getDetails().clientSurName()),
+                () -> assertThat(savedClient.getAge()).isEqualTo(testClientCreateDto.getDetails().age()),
+                () -> assertThat(savedClient.getEmail()).isEqualTo(testClientCreateDto.getEmail()),
+                () -> assertThat(savedClient.getRole()).isEqualTo(Role.USER.name())
+        );
+    }
+
+    @Test
+    void checkCollectionSizeFindAllTest() {
+        List<ClientReadDto> currentList = clientService.findAll();
+        assertThat(currentList.size() >= 5).isTrue();
+    }
+
+    @Test
+    void checkCollectionSizeAfterClientAddFindAllTest() {
+        List<ClientReadDto> currentList = clientService.findAll();
+        int listSize = currentList.size();
+
+        clientService.saveClient(testClientCreateDto);
+
+        List<ClientReadDto> listAfterSaving = clientService.findAll();
+        int sizeListAfterSaving = listAfterSaving.size();
+
+        assertThat(listSize < sizeListAfterSaving).isTrue();
+    }
+
+    @Test
+    void checkExistEmail_ReturnTrue_FindByEmail() {
+        assertThat(clientService.findByEmail(EXIST_EMAIL).isPresent()).isTrue();
+    }
+
+    @Test
+    void checkNotExistEmail_ReturnFalse_FindByEmail() {
+        assertThat(clientService.findByEmail(NON_EXIST_EMAIL).isPresent()).isFalse();
+    }
+}
