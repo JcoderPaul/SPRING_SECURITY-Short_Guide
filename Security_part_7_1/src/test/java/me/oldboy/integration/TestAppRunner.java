7b219d241e807ac7d9ae96199743f88f2d2f8370
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-28 06:38:21 +0500

    Security_part_7_1: Add TestAppRunner.java

---
+++ Security_part_7_1/src/test/java/me/oldboy/integration/TestAppRunner.java
@@ -0,0 +1,8 @@
+package me.oldboy.integration;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration
+// помогает удобно и гибко настраивать Spring-контекст в тестах, не затрагивая продакшен-конфигурацию.
+public class TestAppRunner {
+}
