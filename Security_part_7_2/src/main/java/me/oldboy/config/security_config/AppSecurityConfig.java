cbd0379e7e1ef4dc0243bc68630278246f009851
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-29 22:24:44 +0500

    Security_part_7_2: update AppSecurityConfig.java

--- Security_part_7_2/src/main/java/me/oldboy/config/security_config/AppSecurityConfig.java
+++ Security_part_7_2/src/main/java/me/oldboy/config/security_config/AppSecurityConfig.java
@@ -6,20 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import javax.sql.DataSource;
-
-import java.util.Collections;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
@@ -27,60 +20,62 @@ import static org.springframework.security.web.util.matcher.AntPathRequestMatche
 @Slf4j
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
-@ComponentScan("me.oldboy")
+@ComponentScan("me.oldboy.config.main_config")
 public class AppSecurityConfig {
 
 	@Autowired
-	private DataSource dataSource;
+	private PersistentTokenRepository persistentTokenRepository;
 
 	@Bean
 	@SneakyThrows
-	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) {
-		httpSecurity.authorizeHttpRequests(urlConfig ->
-							urlConfig.requestMatchers(antMatcher("/notices"),
-							                          antMatcher("/contact"),
-										              antMatcher("/static/**"),
-													  antMatcher("/webui/login"),
-											          antMatcher("/webui/registration")).permitAll()
-								     .requestMatchers(antMatcher("/myAccount"),
-									                  antMatcher("/myBalance"),
-										              antMatcher("/myLoans"),
-												      antMatcher("/webui/hello")).authenticated()
-								     .requestMatchers(antMatcher("/admin/**")).hasRole("ADMIN")
-									 .requestMatchers(antMatcher("/myCards")).hasAuthority("READ")
-								     .anyRequest().authenticated())
-				                     .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
-				                     .rememberMe((remember) -> remember.rememberMeParameter("remember-me")
-						                                               .tokenRepository(tokenRepository())
-											 						   .alwaysRemember(true))
-				                     .formLogin(login -> login.loginPage("/webui/login")
-						                                      .defaultSuccessUrl("/webui/hello"));
+	@Order(1)
+	public SecurityFilterChain webFilterChain(HttpSecurity httpSecurity) {
+		httpSecurity
+				.securityMatcher("/webui/**", "/")
+				.authorizeHttpRequests(urlConfig -> urlConfig
+						.requestMatchers(antMatcher("/api/notices"),
+								antMatcher("/api/contact"),
+								antMatcher("/static/**"),
+								antMatcher("/webui/login"),
+								antMatcher("/webui/registration"))
+						.permitAll()
+						.requestMatchers(antMatcher("/webui/myAccount"),
+								antMatcher("/webui/myBalance"),
+								antMatcher("/webui/hello"))
+						.authenticated()
+						.anyRequest().authenticated())
+				.sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
+				.rememberMe((remember) -> remember.rememberMeParameter("remember-me")
+						.tokenRepository(persistentTokenRepository)
+						.alwaysRemember(true))
+				.formLogin(login -> login.loginPage("/webui/login")
+						.defaultSuccessUrl("/webui/hello"));
 
 		return httpSecurity.build();
 	}
 
 	@Bean
-	UrlBasedCorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
-		configuration.setAllowedMethods(Collections.singletonList("*"));
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return source;
-	}
+	@SneakyThrows
+	@Order(5)
+	public SecurityFilterChain restFilterChain(HttpSecurity httpSecurity) {
+		httpSecurity
+				.csrf(AbstractHttpConfigurer::disable)
+				.securityMatcher("/api/**")
+				.authorizeHttpRequests(urlConfig -> urlConfig
+						.requestMatchers(antMatcher("/api/notices"),
+								antMatcher("/api/contact"))
+						.permitAll()
+						.requestMatchers(antMatcher("/api/myAccount"),
+								antMatcher("/api/myBalance"),
+								antMatcher("/api/myLoans"))
+						.authenticated()
+						.requestMatchers(antMatcher("/api/admin/**"))
+						.hasAnyRole("ADMIN")
+						.requestMatchers(antMatcher("/api/myCards"))
+						.hasAuthority("READ")
+						.anyRequest().authenticated())
+				.httpBasic(Customizer.withDefaults());
 
-	@Bean
-	public PersistentTokenRepository tokenRepository() {
-		JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
-		tokenRepository.setDataSource(dataSource);
-		return tokenRepository;
+		return httpSecurity.build();
 	}
-
-	/* Bean который будет помогать в работе с шифрованием/чтением паролей */
-	@Bean
-	public PasswordEncoder getPasswordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
 }
