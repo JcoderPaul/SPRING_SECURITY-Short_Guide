6e23e8c55f3eb7c0deb8e48efe1a4057568d209b
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-29 22:08:53 +0500

    Security_part_7_2: update ContactController.java

--- Security_part_7_2/src/main/java/me/oldboy/controllers/api/ContactController.java
+++ Security_part_7_2/src/main/java/me/oldboy/controllers/api/ContactController.java
@@ -2,15 +2,17 @@ package me.oldboy.controllers.api;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@RequestMapping("/api")
 public class ContactController {
 	
 	@GetMapping("/contact")
-	public String saveContactInquiryDetails() {
-		return "Inquiry details are saved to the DB";
+	public String getContactDetails() {
+		return "Get details from DB";
 	}
 
 }
