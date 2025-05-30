95540e46a6934a5f33a572a5d8a00980ff39c82f
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-30 22:16:04 +0500

    Security_part_9_3: add BalanceController.java

---
+++ Security_part_9_3/src/main/java/me/oldboy/controllers/api/BalanceController.java
@@ -0,0 +1,46 @@
+package me.oldboy.controllers.api;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import me.oldboy.controllers.util.UserDetailsDetector;
+import me.oldboy.dto.transaction_dto.TransactionReadDto;
+import me.oldboy.services.BalanceService;
+import me.oldboy.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@AllArgsConstructor
+@NoArgsConstructor
+public class BalanceController {
+
+	@Autowired
+	private BalanceService balanceService;
+	@Autowired
+	private ClientService clientService;
+	
+	@GetMapping("/myBalance")
+	public ResponseEntity<List<TransactionReadDto>> getBalanceDetails() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		UserDetailsDetector userDetailsDetector = new UserDetailsDetector();
+		ResponseEntity<List<TransactionReadDto>> balanceList = ResponseEntity.noContent().build();
+
+		if (userDetailsDetector.isUserDetailsNotNull(clientService, authentication)) {
+			long clientId = userDetailsDetector.getClientId();
+
+			List<TransactionReadDto> mayBeTransactions = balanceService.readAllTransactionByClientId(clientId);
+			if (mayBeTransactions.size() != 0) {
+				balanceList = ResponseEntity.ok().body(mayBeTransactions);
+			}
+		}
+		return balanceList;
+	}
+}
