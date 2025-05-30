f7b03364132f8e854a1eac2f1dd29be1b7155cfd
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-30 22:18:34 +0500

    Security_part_9_3: add SecurityClientDetails.java

---
+++ Security_part_9_3/src/main/java/me/oldboy/config/securiry_details/SecurityClientDetails.java
@@ -0,0 +1,65 @@
+package me.oldboy.config.securiry_details;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import me.oldboy.models.client.Client;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class SecurityClientDetails implements UserDetails {
+
+	private Client client;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<GrantedAuthority> authorities = new ArrayList<>();
+		authorities.add(new SimpleGrantedAuthority(client.getRole().name()));
+		authorities.addAll(client.getListOfAuth().stream()
+				.map(auth -> new SimpleGrantedAuthority(auth.getAuthName()))
+				.collect(Collectors.toList()));
+		return authorities;
+	}
+
+    /* Два следующих метода объясняют системе безопасности откуда брать имя/пароль для аутентификации */
+	@Override
+	public String getUsername() {
+		return client.getEmail();
+	}
+
+	@Override
+	public String getPassword() {
+		return client.getPass();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+	public Client getClient(){
+		return this.client;
+	}
+}
