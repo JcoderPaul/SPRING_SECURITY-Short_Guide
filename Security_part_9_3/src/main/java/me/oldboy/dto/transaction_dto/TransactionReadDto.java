7b5968301815d3e125e470070893780adeac5f4d
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-30 22:15:04 +0500

    Security_part_9_3: add TransactionReadDto.java

---
+++ Security_part_9_3/src/main/java/me/oldboy/dto/transaction_dto/TransactionReadDto.java
@@ -0,0 +1,49 @@
+package me.oldboy.dto.transaction_dto;
+
+import lombok.*;
+
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class TransactionReadDto {
+    private LocalDate transactionDt;
+    private String transactionSummary;
+    private String transactionType;
+    private Integer transactionAmt;
+    private Integer closingBalance;
+    private LocalDate createDt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TransactionReadDto that = (TransactionReadDto) o;
+        return Objects.equals(transactionDt, that.transactionDt) &&
+                Objects.equals(transactionSummary, that.transactionSummary) &&
+                Objects.equals(transactionType, that.transactionType) &&
+                Objects.equals(transactionAmt, that.transactionAmt) &&
+                Objects.equals(closingBalance, that.closingBalance) &&
+                Objects.equals(createDt, that.createDt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(transactionDt, transactionSummary, transactionType, transactionAmt, closingBalance, createDt);
+    }
+
+    @Override
+    public String toString() {
+        return  "Transaction date: " + transactionDt +
+                ", Transaction summary: " + transactionSummary +
+                ", Transaction type: " + transactionType +
+                ", Transaction amount: " + transactionAmt +
+                ", Closing balance: " + closingBalance +
+                ", Create date: " + createDt;
+    }
+}
