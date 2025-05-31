66ddcf77b0904a2bde5de8ed78691c001c3f18fa
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-31 20:38:58 +0500

    Security_part_7_2: add BalanceControllerTest.java

---
+++ Security_part_7_2/src/test/java/me/oldboy/controllers/api/BalanceControllerTest.java
@@ -0,0 +1,53 @@
+package me.oldboy.controllers.api;
+
+import me.oldboy.config.security_config.AppSecurityConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {AppSecurityConfig.class})
+@WebAppConfiguration
+class BalanceControllerTest {
+
+    @Autowired
+    private WebApplicationContext webAppContext;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext)
+                .apply(springSecurity())    // Включаем Security для MockMvc
+                .build();
+    }
+
+    @Test
+    @WithMockUser
+    void shouldReturnOkGetBalanceDetailsTest() throws Exception {
+        mockMvc.perform(get("/api/myBalance"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Here are the balance details from the DB"));
+    }
+
+    @Test
+    void shouldReturnNotAuthorizedGetBalanceDetailsTest() throws Exception {
+        mockMvc.perform(get("/api/myBalance"))
+                .andExpect(status().isUnauthorized())
+                .andExpect(content().string(""));
+    }
+}
