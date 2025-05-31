c28b5b59f6872dd5d64f2f9417f34089163e6349
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-31 20:39:43 +0500

    Security_part_7_2: add LoansControllerTest.java

---
+++ Security_part_7_2/src/test/java/me/oldboy/controllers/api/LoansControllerTest.java
@@ -0,0 +1,52 @@
+package me.oldboy.controllers.api;
+
+import me.oldboy.config.security_config.AppSecurityConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {AppSecurityConfig.class})
+@WebAppConfiguration
+class LoansControllerTest {
+
+    @Autowired
+    private WebApplicationContext applicationContext;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setAppContext(){
+        mockMvc = MockMvcBuilders.webAppContextSetup(applicationContext)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    @WithMockUser
+    void shouldReturn_200_WithAuthUser_GetLoanDetailsTest() throws Exception {
+        mockMvc.perform(get("/api/myLoans"))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(content().string("Here are the loan details from the DB"));
+    }
+
+    @Test
+    void shouldReturn_401_WithoutAuthUser_GetLoanDetailsTest() throws Exception {
+        mockMvc.perform(get("/api/myLoans"))
+                .andExpect(status().is4xxClientError());
+    }
+}
