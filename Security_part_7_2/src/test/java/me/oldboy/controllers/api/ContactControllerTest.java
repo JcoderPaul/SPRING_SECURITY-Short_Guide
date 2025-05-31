d97ee3d7aaf53f6680f395f2b2e923d111459543
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-05-31 20:39:31 +0500

    Security_part_7_2: add ContactControllerTest.java

---
+++ Security_part_7_2/src/test/java/me/oldboy/controllers/api/ContactControllerTest.java
@@ -0,0 +1,44 @@
+package me.oldboy.controllers.api;
+
+import me.oldboy.config.security_config.AppSecurityConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {AppSecurityConfig.class})
+@WebAppConfiguration
+class ContactControllerTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setWebApplicationContext(){
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    void shouldReturn_200_WithoutAuthUser_GetContactDetailsTest() throws Exception {
+        mockMvc.perform(get("/api/contact"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Get details from DB"));
+    }
+}
