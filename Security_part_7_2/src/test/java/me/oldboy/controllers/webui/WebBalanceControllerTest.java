5fdbecec085452c14d9c87023cdb60a93d3c7257
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-01 18:16:51 +0500

    Security_part_7_2: add WebBalanceControllerTest.java

---
+++ Security_part_7_2/src/test/java/me/oldboy/controllers/webui/WebBalanceControllerTest.java
@@ -0,0 +1,58 @@
+package me.oldboy.controllers.webui;
+
+import lombok.SneakyThrows;
+import me.oldboy.config.security_config.AppSecurityConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {AppSecurityConfig.class})
+@WebAppConfiguration
+class WebBalanceControllerTest {
+
+    @Autowired
+    private WebApplicationContext webAppContext;
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setTestData() {
+        MockitoAnnotations.openMocks(this);
+
+        mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    @SneakyThrows
+    @WithMockUser
+    void showMyBalanceToWebPage_WithAuth_Test() {
+        mockMvc.perform(get("/webui/myBalance"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("My balance from DB"));
+    }
+
+    @Test
+    @SneakyThrows
+    void shouldReturn_3xx_WithoutAuthClient_MyBalanceEndPoint_Test() {
+        mockMvc.perform(get("/webui/myBalance"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(content().string(""))
+                .andExpect(redirectedUrlPattern("**/login"))    // Мы можем указать некий ожидаемый URL паттерн, например, его окончание
+                .andExpect(redirectedUrl("http://localhost/webui/login"));  // Либо мы можем указать точный адрес перенаправления
+    }
+}
