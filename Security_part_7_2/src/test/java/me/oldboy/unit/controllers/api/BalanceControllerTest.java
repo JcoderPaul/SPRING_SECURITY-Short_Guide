5f095a30f03231f73bba08250debd4a06d3b2640
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-01 21:34:56 +0500

    Security_part_7_2: update BalanceControllerTest.java

--- Security_part_7_2/src/test/java/me/oldboy/unit/controllers/api/BalanceControllerTest.java
+++ Security_part_7_2/src/test/java/me/oldboy/unit/controllers/api/BalanceControllerTest.java
@@ -1,4 +1,4 @@
-package me.oldboy.controllers.api;
+package me.oldboy.unit.controllers.api;
 
 import me.oldboy.config.security_config.AppSecurityConfig;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,12 +10,11 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
