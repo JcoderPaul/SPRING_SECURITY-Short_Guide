12ba89345a58c537b024835b32cead5646214888
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-02 16:43:58 +0500

    Security_part_7_2: add TestContainerInit.java

---
+++ Security_part_7_2/src/test/java/me/oldboy/config/test_data_source/TestContainerInit.java
@@ -0,0 +1,21 @@
+package me.oldboy.config.test_data_source;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+public abstract class TestContainerInit {
+    public static final PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:13");
+
+    @BeforeAll
+    static void runContainer() {
+        container.start();
+    }
+
+    @DynamicPropertySource
+    static void postgresProperties(DynamicPropertyRegistry registry) {
+        registry.add("datasource.url", () -> container.getJdbcUrl());
+    }
+}
