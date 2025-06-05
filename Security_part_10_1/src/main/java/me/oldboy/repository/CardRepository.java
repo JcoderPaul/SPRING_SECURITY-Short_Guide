9a2113836d5886b2d1b45f6737ba2cb789c38d59
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-05 07:30:56 +0500

    Security_part_10_1: add CardRepository.java

---
+++ Security_part_10_1/src/main/java/me/oldboy/repository/CardRepository.java
@@ -0,0 +1,14 @@
+package me.oldboy.repository;
+
+import me.oldboy.models.money.Card;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CardRepository extends JpaRepository<Card, Long> {
+
+    Optional<List<Card>> findByClientId(Long clientId);
+}
