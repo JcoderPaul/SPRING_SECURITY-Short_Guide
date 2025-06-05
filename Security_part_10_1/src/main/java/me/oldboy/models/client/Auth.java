97a1c229010d3fd15847df6cd167bbe02bff9603
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-05 07:28:47 +0500

    Security_part_10_1: add Auth.java

---
+++ Security_part_10_1/src/main/java/me/oldboy/models/client/Auth.java
@@ -0,0 +1,37 @@
+package me.oldboy.models.client;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "authorities")
+public class Auth {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long Id;
+
+    @Column(name = "authority_name")
+    private String authName;
+
+    @ManyToMany(fetch = FetchType.LAZY,
+                cascade = CascadeType.PERSIST)
+    @JoinTable(name = "clients_authorities",
+               joinColumns = @JoinColumn(name = "authority_id"),
+               inverseJoinColumns = @JoinColumn(name = "client_id"))
+    @Builder.Default
+    private List<Client> listOfClients = new ArrayList<>();
+
+    public void addClientToList(Client client){
+        listOfClients.add(client);
+    }
+}
