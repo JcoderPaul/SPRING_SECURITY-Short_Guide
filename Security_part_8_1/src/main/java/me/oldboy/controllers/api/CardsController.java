83283cfa47445b51bc091ee8b1c796ede5065eea
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-05 21:48:46 +0500

    Security_part_8_1: update CardsController.java

--- Security_part_8_1/src/main/java/me/oldboy/controllers/api/CardsController.java
+++ Security_part_8_1/src/main/java/me/oldboy/controllers/api/CardsController.java
@@ -2,14 +2,16 @@ package me.oldboy.controllers.api;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@RequestMapping("/api")
 public class CardsController {
 	
 	@GetMapping("/myCards")
-	public String getCardDetails(String input) {
+	public String getCardDetails() {
 		return "Here are the card details from the DB";
 	}
 
