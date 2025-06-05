4e77ff4cc8c4fc9214ee8e9c4652efe8b40581c8
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-05 21:48:31 +0500

    Security_part_8_1: update BalanceController.java

--- Security_part_8_1/src/main/java/me/oldboy/controllers/api/BalanceController.java
+++ Security_part_8_1/src/main/java/me/oldboy/controllers/api/BalanceController.java
@@ -2,14 +2,16 @@ package me.oldboy.controllers.api;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@RequestMapping("/api")
 public class BalanceController {
 	
 	@GetMapping("/myBalance")
-	public String getBalanceDetails(String input) {
+	public String getBalanceDetails() {
 		return "Here are the balance details from the DB";
 	}
 
