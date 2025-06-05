5e859f22b25723d5386ef74a9a8db2dd92f3b4f5
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-05 07:18:20 +0500

    Security_part_10_1: add AdminController.java

---
+++ Security_part_10_1/src/main/java/me/oldboy/controllers/api/AdminController.java
@@ -0,0 +1,29 @@
+package me.oldboy.controllers.api;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import me.oldboy.dto.client_dto.ClientReadDto;
+import me.oldboy.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@AllArgsConstructor
+@NoArgsConstructor
+@RequestMapping("/admin") // Задаем общий префикс для всех эндпоинтов этого контроллера
+public class AdminController {
+
+    @Autowired
+    private ClientService clientService;
+
+    @GetMapping("/getAllClient")
+    public List<ClientReadDto> getAllClient(){
+        return clientService.findAll();
+    }
+}
