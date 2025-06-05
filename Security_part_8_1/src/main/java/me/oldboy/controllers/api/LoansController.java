9ffca894e77673bd153d4683356bb5869bffb7f1
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-05 21:49:36 +0500

    Security_part_8_1: update LoansController.java

--- Security_part_8_1/src/main/java/me/oldboy/controllers/api/LoansController.java
+++ Security_part_8_1/src/main/java/me/oldboy/controllers/api/LoansController.java
@@ -2,14 +2,16 @@ package me.oldboy.controllers.api;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@RequestMapping("/api")
 public class LoansController {
 	
 	@GetMapping("/myLoans")
-	public String getLoanDetails(String input) {
+	public String getLoanDetails() {
 		return "Here are the loan details from the DB";
 	}
 
