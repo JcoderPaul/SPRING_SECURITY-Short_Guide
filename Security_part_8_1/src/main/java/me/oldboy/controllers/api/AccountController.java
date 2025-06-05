f12d863dc6bcc917d49b4f8a2d3e6d793b99938c
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-05 21:48:15 +0500

    Security_part_8_1: update AccountController.java

--- Security_part_8_1/src/main/java/me/oldboy/controllers/api/AccountController.java
+++ Security_part_8_1/src/main/java/me/oldboy/controllers/api/AccountController.java
@@ -2,14 +2,16 @@ package me.oldboy.controllers.api;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@RequestMapping("/api")
 public class AccountController {
 	
 	@GetMapping("/myAccount")
-	public String getAccountDetails(String input) {
+	public String getAccountDetails() {
 		return "Here are the account details from the DB";
 	}
 
