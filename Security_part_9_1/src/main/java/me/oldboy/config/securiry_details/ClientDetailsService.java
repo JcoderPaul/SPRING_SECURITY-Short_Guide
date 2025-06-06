6d573cda7c1e37f95d3d72c0b63d789b7ef4a4c9
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-06 21:43:34 +0500

    Security_part_9_1: update ClientDetailsService.java

--- Security_part_9_1/src/main/java/me/oldboy/config/securiry_details/ClientDetailsService.java
+++ Security_part_9_1/src/main/java/me/oldboy/config/securiry_details/ClientDetailsService.java
@@ -6,6 +6,7 @@ import me.oldboy.exception.EmailNotFoundException;
 import me.oldboy.models.client.Client;
 import me.oldboy.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
