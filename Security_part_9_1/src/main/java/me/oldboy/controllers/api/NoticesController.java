749b23e7a362d9f87ce0403d3edfa8692ea56e1c
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-08 21:04:56 +0500

    Security_part_9_1: update NoticesController.java

--- Security_part_9_1/src/main/java/me/oldboy/controllers/api/NoticesController.java
+++ Security_part_9_1/src/main/java/me/oldboy/controllers/api/NoticesController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
+@RequestMapping("/api")
 public class NoticesController {
 	@RequestMapping(value = "/notices", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<String>> getNotices() throws IOException {
