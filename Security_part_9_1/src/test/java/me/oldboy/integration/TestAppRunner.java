46509466bf56be0878895499300059f0ac3cd08e
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-11 07:34:01 +0500

    Security_part_9_1: add TestAppRunner.java

---
+++ Security_part_9_1/src/test/java/me/oldboy/integration/TestAppRunner.java
@@ -0,0 +1,10 @@
+package me.oldboy.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration
+public class TestAppRunner {
+
+}
