b13102ea19c9da4b335eee87f54f681b13ac3414
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-12 17:11:31 +0500

    Security_part_9_1: update name LogoutControllerTestIT.java

--- Security_part_9_1/src/test/java/me/oldboy/integration/controllers/webui/LogoutControllerTestIT.java
+++ Security_part_9_1/src/test/java/me/oldboy/integration/controllers/webui/LogoutControllerTestIT.java
@@ -27,7 +27,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @AutoConfigureMockMvc
-class LogoutControllerTest extends IntegrationTestBase {
+class LogoutControllerTestIT extends IntegrationTestBase {
 
     @Autowired
     private LogoutController logoutController;
