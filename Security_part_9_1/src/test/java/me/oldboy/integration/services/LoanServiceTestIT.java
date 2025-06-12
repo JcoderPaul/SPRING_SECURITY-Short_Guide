47714d313ca642ebd532111d689885e4b7dc0913
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-12 06:25:56 +0500

    Security_part_9_1: add LoanServiceTestIT.java

---
+++ Security_part_9_1/src/test/java/me/oldboy/integration/services/LoanServiceTestIT.java
@@ -0,0 +1,35 @@
+package me.oldboy.integration.services;
+
+import lombok.RequiredArgsConstructor;
+import me.oldboy.dto.loan_dto.LoanReadDto;
+import me.oldboy.integration.IntegrationTestBase;
+import me.oldboy.services.LoanService;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RequiredArgsConstructor
+class LoanServiceTestIT extends IntegrationTestBase {
+
+    private final LoanService loanService;
+
+    private Long clientId;
+
+    @Test
+    void shouldReturnTrue_LoanReadDtoList_ReadAllLoansByUserId_Test() {
+        clientId = 1L;
+        List<LoanReadDto> allClientLoans = loanService.readAllLoansByUserId(clientId);
+
+        assertThat(allClientLoans.size() > 0).isTrue();
+    }
+
+    @Test
+    void shouldReturnEmptyList_NotExistClient_ReadAllLoansByUserId_Test() {
+        clientId = 10L;
+        List<LoanReadDto> allClientLoans = loanService.readAllLoansByUserId(clientId);
+
+        assertThat(allClientLoans.size()).isEqualTo(0);
+    }
+}
