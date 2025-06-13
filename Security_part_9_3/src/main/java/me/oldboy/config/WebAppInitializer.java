ea21b5412480b49a8f4ac5b89ffa7eb659fad040
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-14 00:11:30 +0500

    Security_part_9_3: update WebAppInitializer.java

--- Security_part_9_3/src/main/java/me/oldboy/config/WebAppInitializer.java
+++ Security_part_9_3/src/main/java/me/oldboy/config/WebAppInitializer.java
@@ -1,25 +1,19 @@
 package me.oldboy.config;
 
 import lombok.extern.slf4j.Slf4j;
-import me.oldboy.config.security_config.AppSecurityConfig;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import me.oldboy.config.main_config.MainAppConfig;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 @Slf4j
-@Configuration
-@EnableWebMvc
-@ComponentScan(basePackages = "me.oldboy")
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[]{AppSecurityConfig.class};
+        return new Class[]{MainAppConfig.class};
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[]{WebAppInitializer.class};
+        return new Class[]{};
     }
 
     @Override
