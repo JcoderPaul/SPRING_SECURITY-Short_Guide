3eea82feab159652ac51b9d9ef5abe53a42274ab
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-14 00:11:13 +0500

    Security_part_9_3: add AppSecurityInitializer.java

---
+++ Security_part_9_3/src/main/java/me/oldboy/config/AppSecurityInitializer.java
@@ -0,0 +1,6 @@
+package me.oldboy.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class AppSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
