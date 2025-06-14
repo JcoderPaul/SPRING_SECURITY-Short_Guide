11bfa46ce264f37c23cf6c70cb290a5d9d2c720c
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-14 07:25:08 +0500

    Security_part_9_3: update LoansController.java

--- Security_part_9_3/src/main/java/me/oldboy/controllers/api/LoansController.java
+++ Security_part_9_3/src/main/java/me/oldboy/controllers/api/LoansController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,17 +21,19 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @NoArgsConstructor
+@RequestMapping("/api")
 public class LoansController {
 
 	@Autowired
 	private ClientService clientService;
 	@Autowired
 	private LoanService loanService;
+	@Autowired
+	private UserDetailsDetector userDetailsDetector;
 	
 	@GetMapping("/myLoans")
 	public ResponseEntity<List<LoanReadDto>> getLoanDetails() {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		UserDetailsDetector userDetailsDetector = new UserDetailsDetector();
 		ResponseEntity<List<LoanReadDto>> loansList = ResponseEntity.noContent().build();
 
 		if(userDetailsDetector.isUserDetailsNotNull(clientService, authentication)){
