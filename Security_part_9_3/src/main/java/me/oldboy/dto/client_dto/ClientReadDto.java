cd29923678e0414b5ee8a90b3de2db9547cdc539
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-14 07:25:56 +0500

    Security_part_9_3: update ClientReadDto.java

--- Security_part_9_3/src/main/java/me/oldboy/dto/client_dto/ClientReadDto.java
+++ Security_part_9_3/src/main/java/me/oldboy/dto/client_dto/ClientReadDto.java
@@ -4,8 +4,6 @@ import lombok.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
 @Builder
 public class ClientReadDto {
     private Long id;
@@ -22,5 +20,53 @@ public class ClientReadDto {
                " Name: " + clientName +
                " Surname: " + clientSurName +
                " Age: " + age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public String getClientSurName() {
+        return clientSurName;
+    }
+
+    public void setClientSurName(String clientSurName) {
+        this.clientSurName = clientSurName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
     }
 }
