32697c4a5688ccb2809f928f4cffec266d92bdc0
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-15 08:06:39 +0500

    Security_part_9_3: update ClientDetailsServiceException.java

--- Security_part_9_3/src/main/java/me/oldboy/exception/handlers/ClientDetailsServiceException.java
+++ Security_part_9_3/src/main/java/me/oldboy/exception/handlers/ClientDetailsServiceException.java
@@ -1,6 +1,10 @@
 package me.oldboy.exception.handlers;
 
 import lombok.extern.slf4j.Slf4j;
+import me.oldboy.exception.AccountServiceException;
+import me.oldboy.exception.ClientServiceException;
+import me.oldboy.exception.DuplicateClientEmailException;
+import me.oldboy.exception.EmailNotFoundException;
 import me.oldboy.exception.exception_entity.ExceptionResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +15,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ClientDetailsServiceException {
 
-    @ExceptionHandler(RuntimeException.class)
+    /*
+    Это очень грубо, когда мы повесили на обработчик "всех собак", что
+    исключения, что их обработчики должны быть специализированными, но
+    в данном случае мы изучаем безопасность, а не обработчики исключений.
+    */
+    @ExceptionHandler({
+            AccountServiceException.class,
+            ClientServiceException.class,
+            DuplicateClientEmailException.class,
+            EmailNotFoundException.class
+    })
     public ResponseEntity<ExceptionResponse> handleExceptions(RuntimeException exception) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(exception.getMessage());
         return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
