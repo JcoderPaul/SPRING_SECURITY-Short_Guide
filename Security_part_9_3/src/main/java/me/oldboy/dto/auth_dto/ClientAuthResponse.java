f79345e402ed449839095bbc3324bd4301c8287c
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-15 12:26:24 +0500

    Security_part_9_3: update ClientAuthResponse.java

--- Security_part_9_3/src/main/java/me/oldboy/dto/auth_dto/ClientAuthResponse.java
+++ Security_part_9_3/src/main/java/me/oldboy/dto/auth_dto/ClientAuthResponse.java
@@ -2,13 +2,61 @@ package me.oldboy.dto.auth_dto;
 
 import lombok.*;
 
+import java.util.Objects;
+
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
+@NoArgsConstructor
 public class ClientAuthResponse {
     private Long id;
     private String clientLogin;
     private String accessToken;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getClientLogin() {
+        return clientLogin;
+    }
+
+    public void setClientLogin(String clientLogin) {
+        this.clientLogin = clientLogin;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    @Override
+    public String toString() {
+        return "ClientAuthResponse{" +
+                "id=" + id +
+                ", clientLogin='" + clientLogin + '\'' +
+                ", accessToken='" + accessToken + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ClientAuthResponse that = (ClientAuthResponse) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(clientLogin, that.clientLogin) &&
+                Objects.equals(accessToken, that.accessToken);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, clientLogin, accessToken);
+    }
 }
