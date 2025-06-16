fecf29230b1018451605542874098a12b23e1279
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-16 08:09:18 +0500

    Security_part_9_2: add ContactRepositoryTestIT.java

---
+++ Security_part_9_2/src/test/java/me/oldboy/integration/repository/ContactRepositoryTestIT.java
@@ -0,0 +1,43 @@
+package me.oldboy.integration.repository;
+
+import lombok.RequiredArgsConstructor;
+import me.oldboy.integration.IntegrationTestBaseConnection;
+import me.oldboy.models.client_info.Contact;
+import me.oldboy.repository.ContactRepository;
+import org.junit.jupiter.api.*;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@RequiredArgsConstructor
+class ContactRepositoryTestIT extends IntegrationTestBaseConnection {
+
+    private final ContactRepository contactRepository;
+
+    private Long userId;
+
+    @Test
+    @DisplayName("Test 1: Find contact data by client ID from DB")
+    @Order(1)
+    @Rollback(value = false)
+    void shouldReturnTrue_IfFindContactByClientId_Test() {
+        userId = 1L;
+        Optional<Contact> mayBeContact = contactRepository.findByClientId(userId);
+
+        assertThat(mayBeContact.isPresent()).isTrue();
+    }
+
+    @Test
+    @DisplayName("Test 2: Can not find contact data by client ID from DB")
+    @Order(2)
+    @Rollback(value = false)
+    void shouldReturnFalse_IfNotFindContactByClientId_Test() {
+        userId = 10L;
+        Optional<Contact> mayBeContact = contactRepository.findByClientId(userId);
+
+        assertThat(mayBeContact.isPresent()).isFalse();
+    }
+}
