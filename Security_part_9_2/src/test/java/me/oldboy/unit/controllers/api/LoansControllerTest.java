3db3b7972027a85dbf8a7723c5a61cac607f847e
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-16 08:03:10 +0500

    Security_part_9_2: add LoansControllerTest.java

---
+++ Security_part_9_2/src/test/java/me/oldboy/unit/controllers/api/LoansControllerTest.java
@@ -0,0 +1,50 @@
+package me.oldboy.unit.controllers.api;
+
+import me.oldboy.controllers.api.LoansController;
+import me.oldboy.repository.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.sql.DataSource;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(LoansController.class)
+class LoansControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private DataSource dataSource;
+    @MockBean
+    private ClientRepository clientRepository;
+    @MockBean
+    private ContactRepository contactRepository;
+    @MockBean
+    private BalanceRepository balanceRepository;
+    @MockBean
+    private CardRepository cardRepository;
+    @MockBean
+    private LoanRepository loanRepository;
+
+    @Test
+    @WithMockUser
+    void shouldReturn_200_WithAuthUser_GetLoanDetailsTest() throws Exception {
+        mockMvc.perform(get("/api/myLoans"))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(content().contentType("text/plain;charset=UTF-8"))
+                .andExpect(content().string("Here are the loan details from the DB"));
+    }
+
+    @Test
+    void shouldReturn_3xx_WithoutAuthUser_GetLoanDetailsTest() throws Exception {
+        mockMvc.perform(get("/api/myLoans"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(content().string(""))
+                .andExpect(redirectedUrlPattern("**/webui/login"));
+    }
+}
