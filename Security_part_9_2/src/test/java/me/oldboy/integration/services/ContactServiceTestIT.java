a9dd9afcd42892d57434a6e915978aa98f348352
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-16 08:10:40 +0500

    Security_part_9_2: add ContactServiceTestIT.java

---
+++ Security_part_9_2/src/test/java/me/oldboy/integration/services/ContactServiceTestIT.java
@@ -0,0 +1,35 @@
+package me.oldboy.integration.services;
+
+import lombok.RequiredArgsConstructor;
+import me.oldboy.dto.contact_dto.ContactReadDto;
+import me.oldboy.integration.IntegrationTestBaseConnection;
+import me.oldboy.services.ContactService;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RequiredArgsConstructor
+class ContactServiceTestIT extends IntegrationTestBaseConnection {
+
+    private final ContactService contactService;
+
+    private Long clientId;
+
+    @Test
+    void shouldReturnTrue_OptionalContactReadDto_ReadContact_Test() {
+        clientId = 1L;
+        Optional<ContactReadDto> findContact = contactService.readContact(clientId);
+
+        assertThat(findContact.isPresent()).isTrue();
+    }
+
+    @Test
+    void shouldReturnOptionalEmpty_NotExistClient_ReadContact_Test() {
+        clientId = 10L;
+        Optional<ContactReadDto> findContact = contactService.readContact(clientId);
+
+        assertThat(findContact.isEmpty()).isTrue();
+    }
+}
