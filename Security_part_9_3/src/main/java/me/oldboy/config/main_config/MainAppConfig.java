ef7d5d446646f647ff3ead272b882da9256063ae
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-21 21:09:48 +0500

    Security_part_9_3: update MainAppConfig.java

--- Security_part_9_3/src/main/java/me/oldboy/config/main_config/MainAppConfig.java
+++ Security_part_9_3/src/main/java/me/oldboy/config/main_config/MainAppConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan({
+@ComponentScan(basePackages = {
         "me.oldboy.controllers"
         ,"me.oldboy.services"
         ,"me.oldboy.repository"
