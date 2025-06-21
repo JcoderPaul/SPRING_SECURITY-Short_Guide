89a96867a804601a99bca2e8644876dcc8a9dc70
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-21 21:13:21 +0500

    Security_part_9_3: update CardsControllerTestIT.java

--- Security_part_9_3/src/test/java/me/oldboy/integration/controllers/CardsControllerTestIT.java
+++ Security_part_9_3/src/test/java/me/oldboy/integration/controllers/CardsControllerTestIT.java
@@ -14,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -23,7 +22,7 @@ import java.util.List;
 import static me.oldboy.test_constant.TestConstantFields.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
