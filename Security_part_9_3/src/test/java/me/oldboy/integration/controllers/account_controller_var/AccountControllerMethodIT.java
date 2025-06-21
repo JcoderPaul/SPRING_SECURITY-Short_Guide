43097437cc407f9bed7b0f645bcee9c08c261c0f
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-21 10:11:17 +0500

    Security_part_9_3: update AccountControllerMethodIT.java

--- Security_part_9_3/src/test/java/me/oldboy/integration/controllers/account_controller_var/AccountControllerMethodIT.java
+++ Security_part_9_3/src/test/java/me/oldboy/integration/controllers/account_controller_var/AccountControllerMethodIT.java
@@ -5,23 +5,13 @@ import me.oldboy.config.test_data_source.TestContainerInit;
 import me.oldboy.controllers.api.AccountController;
 import me.oldboy.dto.account_dto.AccountReadDto;
 import me.oldboy.integration.annotation.IT;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-import java.util.List;
 
 import static me.oldboy.test_constant.TestConstantFields.*;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @IT
 class AccountControllerMethodIT extends TestContainerInit {
