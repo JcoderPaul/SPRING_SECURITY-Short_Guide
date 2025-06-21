69a7540bcf35a6f741004bd08d754f767d26ee2a
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-21 16:45:18 +0500

    Security_part_9_3: update BalanceControllerTestIT.java

--- Security_part_9_3/src/test/java/me/oldboy/integration/controllers/BalanceControllerTestIT.java
+++ Security_part_9_3/src/test/java/me/oldboy/integration/controllers/BalanceControllerTestIT.java
@@ -8,15 +8,12 @@ import me.oldboy.config.test_data_source.TestContainerInit;
 import me.oldboy.controllers.api.BalanceController;
 import me.oldboy.dto.transaction_dto.TransactionReadDto;
 import me.oldboy.integration.annotation.IT;
-import me.oldboy.test_constant.TestConstantFields;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -25,7 +22,7 @@ import java.util.List;
 import static me.oldboy.test_constant.TestConstantFields.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
