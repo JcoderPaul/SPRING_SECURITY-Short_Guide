4382b023ea6410a6b49f88198a7382175afd4b04
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-21 20:33:39 +0500

    Security_part_9_3: add ClientControllerTestIT.java

---
+++ Security_part_9_3/src/test/java/me/oldboy/integration/controllers/ClientControllerTestIT.java
@@ -0,0 +1,221 @@
+package me.oldboy.integration.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import me.oldboy.config.test_data_source.TestContainerInit;
+import me.oldboy.dto.auth_dto.ClientAuthRequest;
+import me.oldboy.dto.auth_dto.ClientAuthResponse;
+import me.oldboy.dto.client_dto.ClientCreateDto;
+import me.oldboy.dto.client_dto.ClientReadDto;
+import me.oldboy.dto.details_dto.DetailsCreateDto;
+import me.oldboy.integration.annotation.IT;
+import me.oldboy.models.client.Client;
+import me.oldboy.services.ClientService;
+import me.oldboy.test_constant.TestConstantFields;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.List;
+import java.util.Optional;
+
+import static me.oldboy.test_constant.TestConstantFields.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@IT
+class ClientControllerTestIT extends TestContainerInit {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    @Autowired
+    private ClientService clientService;
+    private MockMvc mockMvc;
+    private ClientCreateDto testClientCreateDto, notValidClientCreateDto, existClientCreateDto;
+    private DetailsCreateDto testDetailsCreateDto, notValidClientDetailsDto;
+    private ObjectMapper objectMapper;
+    private ClientAuthRequest testClientAuthRequest, notValidAuthRequest, notExistingAuthRequest;
+
+    @BeforeEach
+    void setUp(){
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(springSecurity())
+                .build();
+
+        objectMapper = new ObjectMapper();
+
+        /* Валидные данные для сохранения при регистрации */
+        testDetailsCreateDto = DetailsCreateDto.builder()
+                .clientName(TEST_CLIENT_NAME)
+                .clientSurName(TEST_CLIENT_SUR_NAME)
+                .age(TEST_AGE)
+                .build();
+        testClientCreateDto = ClientCreateDto.builder()
+                .email(TEST_EMAIL)
+                .password(TEST_PASS)
+                .details(testDetailsCreateDto)
+                .build();
+        existClientCreateDto = ClientCreateDto.builder()
+                .email(EXIST_EMAIL)
+                .password(TEST_PASS)
+                .details(testDetailsCreateDto)
+                .build();
+
+        /* НЕ валидные данные для проверки валидатора при регистрации */
+        notValidClientDetailsDto = DetailsCreateDto.builder()
+                .clientName("A")
+                .clientSurName("A")
+                .age(-5)
+                .build();
+        notValidClientCreateDto = ClientCreateDto.builder()
+                .email("er")
+                .password("1")
+                .details(notValidClientDetailsDto)
+                .build();
+
+        /* Аутентификация верными данными */
+        testClientAuthRequest = ClientAuthRequest.builder()
+                .username(EXIST_EMAIL)
+                .password(TEST_PASS)
+                .build();
+        notValidAuthRequest = ClientAuthRequest.builder()
+                .username("wer")
+                .password("1")
+                .build();
+        notExistingAuthRequest = ClientAuthRequest.builder()
+                .username(TEST_EMAIL)
+                .password(TEST_PASS)
+                .build();
+    }
+
+    @Test
+    @SneakyThrows
+    void registrationClient_ValidRegData_ShouldReturnOk_AndSavedClientDto_Test() {
+        String requestBodyData = objectMapper.writeValueAsString(testClientCreateDto);  // Подготовим данные для сохранения
+        List<ClientReadDto> beforeSaveBase = clientService.findAll();   // Получим копию списка клиентов до сохранения
+
+        /* Имитируем запрос на сохранение клиента */
+        MvcResult result = mockMvc.perform(post("/api/regClient")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBodyData))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        List<ClientReadDto> afterSaveBase = clientService.findAll();    // Получим копию списка клиентов после сохранения
+
+        /* Получим результат запроса, обработаем до нужного формата и сравним с исходным */
+        String strReturn = result.getResponse().getContentAsString();
+        ClientReadDto savedClientDto= objectMapper.readValue(strReturn, ClientReadDto.class);
+
+        assertAll(
+                () -> assertThat(savedClientDto.getEmail()).isEqualTo(testClientCreateDto.getEmail()),
+                () -> assertThat(savedClientDto.getClientName()).isEqualTo(testClientCreateDto.getDetails().getClientName()),
+                () -> assertThat(savedClientDto.getClientSurName()).isEqualTo(testClientCreateDto.getDetails().getClientSurName()),
+                () -> assertThat(savedClientDto.getAge()).isEqualTo(testClientCreateDto.getDetails().getAge())
+        );
+
+        assertThat(afterSaveBase.size()).isGreaterThan(beforeSaveBase.size());  // Сравним размер списка клиентов до и после сохранения
+    }
+
+    @Test
+    @SneakyThrows
+    void registrationClient_NotValidRegData_ShouldReturnBadRequest_AndErrorWarnings_Test() {
+        String requestBodyData = objectMapper.writeValueAsString(notValidClientCreateDto);
+
+        MvcResult result = mockMvc.perform(post("/api/regClient")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBodyData))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+
+        String strReturn = result.getResponse().getContentAsString();
+
+        assertAll(
+                () -> assertThat(strReturn.contains("Standard e-mail structure - email_name@email_domain.top_lavel_domain (for example: paul@tradsystem.ru)")).isTrue(),
+                () -> assertThat(strReturn.contains("Age can't be lass then 0, unless you come from a counter-directional universe!")).isTrue(),
+                () -> assertThat(strReturn.contains("Password size can be between 2 and 64")).isTrue()
+        );
+    }
+
+    @Test
+    @SneakyThrows
+    void registrationClient_TryToDuplicateReg_ShouldReturnBadRequest_AndErrorWarnings_Test() {
+        String requestBodyData = objectMapper.writeValueAsString(existClientCreateDto);
+
+        MvcResult result = mockMvc.perform(post("/api/regClient")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBodyData))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+
+        String strReturn = result.getResponse().getContentAsString();
+
+        assertThat(strReturn.contains("Email: " + EXIST_EMAIL + " is exist, can not duplicate data.")).isTrue();
+    }
+
+    @Test
+    @SneakyThrows
+    void loginClient_ShouldReturnCorrectClientAuthResponse_Test() {
+        String requestBodyData = objectMapper.writeValueAsString(testClientAuthRequest);
+
+        MvcResult result = mockMvc.perform(post("/api/loginClient")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBodyData))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String strReturn = result.getResponse().getContentAsString();
+
+        ClientAuthResponse clientAuthResponse = objectMapper.readValue(strReturn, ClientAuthResponse.class);
+        assertAll(
+                () -> assertThat(strReturn.contains("id")).isTrue(),
+                () -> assertThat(strReturn.contains("clientLogin")).isTrue(),
+                () -> assertThat(strReturn.contains("accessToken")).isTrue(),
+                () -> assertThat(clientAuthResponse.getClientLogin()).isEqualTo(testClientAuthRequest.getUsername())
+        );
+    }
+
+    @Test
+    @SneakyThrows
+    void loginClient_NotValidData_ShouldReturnBadRequest_AndErrorWarnings_Test() {
+        String requestBodyData = objectMapper.writeValueAsString(notValidAuthRequest);
+
+        MvcResult result = mockMvc.perform(post("/api/loginClient")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBodyData))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+
+        String strReturn = result.getResponse().getContentAsString();
+
+        assertAll(
+                () -> assertThat(strReturn.contains("Email size can be between 5 and 64")).isTrue(),
+                () -> assertThat(strReturn.contains("Password size can be between 2 and 64")).isTrue(),
+                () -> assertThat(strReturn.contains("Standard e-mail structure - email_name@email_domain.top_lavel_domain (for example: paul@tradsystem.ru)")).isTrue()
+        );
+    }
+
+    @Test
+    @SneakyThrows
+    void loginClient_NotExistClientLogin_ShouldReturnBadRequest_AndErrorWarnings_Test() {
+        String requestBodyData = objectMapper.writeValueAsString(notExistingAuthRequest);
+
+        MvcResult result = mockMvc.perform(post("/api/loginClient")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBodyData))
+                .andExpect(status().is4xxClientError())
+                .andReturn();
+
+        String strReturn = result.getResponse().getContentAsString();
+
+        assertThat(strReturn.contains("User name (email) not correct!")).isTrue();
+    }
+}
