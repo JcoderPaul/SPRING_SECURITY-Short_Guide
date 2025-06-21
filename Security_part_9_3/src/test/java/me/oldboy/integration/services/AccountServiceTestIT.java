e06cc97c339f6a89fbab79e32d3e2093c0d4e3c1
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-21 21:14:17 +0500

    Security_part_9_3: update AccountServiceTestIT.java

--- Security_part_9_3/src/test/java/me/oldboy/integration/services/AccountServiceTestIT.java
+++ Security_part_9_3/src/test/java/me/oldboy/integration/services/AccountServiceTestIT.java
@@ -5,7 +5,6 @@ import me.oldboy.dto.account_dto.AccountReadDto;
 import me.oldboy.exception.AccountServiceException;
 import me.oldboy.integration.annotation.IT;
 import me.oldboy.services.AccountService;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
