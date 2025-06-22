bb7f8b6dc86a8a1798e1abf9f5eef0b4b6bde470
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-22 20:33:35 +0500

    Security_part_10_1: update AccountController.java

--- Security_part_10_1/src/main/java/me/oldboy/controllers/api/AccountController.java
+++ Security_part_10_1/src/main/java/me/oldboy/controllers/api/AccountController.java
@@ -10,10 +10,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@RequestMapping("/api")
 public class AccountController {
 
 	@Autowired
@@ -21,7 +23,7 @@ public class AccountController {
 	@Autowired
 	private ClientService clientService;
 
-	@GetMapping("/myAccountInfo")
+	@GetMapping("/myAccount")
 	public ResponseEntity<AccountReadDto> getAccountDetails() {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		UserDetailsDetector userDetailsDetector = new UserDetailsDetector();
