8a36d7da2a26be0678abd398de8f8d41e326de8d
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-22 20:34:20 +0500

    Security_part_10_1: update ClientController.java

--- Security_part_10_1/src/main/java/me/oldboy/controllers/api/ClientController.java
+++ Security_part_10_1/src/main/java/me/oldboy/controllers/api/ClientController.java
@@ -14,6 +14,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -23,6 +24,7 @@ import java.util.stream.Collectors;
 @RestController
 @AllArgsConstructor
 @NoArgsConstructor
+@RequestMapping("/api")
 public class ClientController {
 
     @Autowired
