94155fb31b05ad000a179f4cef61efdedfac805f
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-22 20:33:58 +0500

    Security_part_10_1: update BalanceController.java

--- Security_part_10_1/src/main/java/me/oldboy/controllers/api/BalanceController.java
+++ Security_part_10_1/src/main/java/me/oldboy/controllers/api/BalanceController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,6 +21,7 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @NoArgsConstructor
+@RequestMapping("/api")
 public class BalanceController {
 
 	@Autowired
