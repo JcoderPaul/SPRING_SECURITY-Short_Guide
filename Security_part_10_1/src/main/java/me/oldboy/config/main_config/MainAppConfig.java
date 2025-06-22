b3ac226aca0e43dcf496fe5ae706bb959f439e4d
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-22 20:50:26 +0500

    Security_part_10_1: update MainAppConfig.java

---
+++ Security_part_10_1/src/main/java/me/oldboy/config/main_config/MainAppConfig.java
@@ -0,0 +1,20 @@
+package me.oldboy.config.main_config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {
+        "me.oldboy.controllers"
+        ,"me.oldboy.services"
+        ,"me.oldboy.repository"
+        ,"me.oldboy.exception"
+        ,"me.oldboy.config.data_source"
+        ,"me.oldboy.config.security_config"
+        ,"me.oldboy.config.security_details"
+        ,"me.oldboy.config.auth_event_listener"
+})
+public class MainAppConfig {
+}
