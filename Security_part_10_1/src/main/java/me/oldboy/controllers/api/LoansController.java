28c04bd43eb47ceb5932a3e216ee87e9ded9f484
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-22 19:39:18 +0500

    Security_part_10_1: update LoansController.java

--- Security_part_10_1/src/main/java/me/oldboy/controllers/api/LoansController.java
+++ Security_part_10_1/src/main/java/me/oldboy/controllers/api/LoansController.java
@@ -105,8 +105,12 @@ public class LoansController {
 	*/
 	@PreFilter(filterTarget = "dtoList", value = "filterObject.clientId == #userDetails.client.id")
 	@PostMapping("/save-all-loans")
-	public ResponseEntity<String> saveAllMyRequestLoan(@Param("dtoList") @RequestBody List<LoanCreateDto> dtoList,
-														@Param("userDetails") @AuthenticationPrincipal SecurityClientDetails userDetails){
+	public ResponseEntity<String> saveAllMyRequestLoan(@Param("dtoList")
+													   @RequestBody
+													   List<LoanCreateDto> dtoList,
+													   @Param("userDetails")
+													   @AuthenticationPrincipal
+													   SecurityClientDetails userDetails){
 
 		String prnResponseAsSting = "Operation is failed!";
 		if(loanService.saveAllMyLoans(dtoList, userDetails)){
