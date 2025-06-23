44c40d4fc097d2826b325b6b1829b1a532613336
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-23 22:25:50 +0500

    Security_part_10_1: add SecurityClientDetailsException.java

---
+++ Security_part_10_1/src/main/java/me/oldboy/exception/SecurityClientDetailsException.java
@@ -0,0 +1,7 @@
+package me.oldboy.exception;
+
+public class SecurityClientDetailsException extends RuntimeException {
+    public SecurityClientDetailsException(String msg) {
+        super(msg);
+    }
+}
