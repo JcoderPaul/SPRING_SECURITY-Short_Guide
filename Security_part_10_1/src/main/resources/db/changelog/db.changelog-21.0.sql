c27b059e7a109d50efb4bd833c23cdade8f42b54
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-24 05:28:50 +0500

    Security_part_10_1: add db.changelog-21.0.sql

---
+++ Security_part_10_1/src/main/resources/db/changelog/db.changelog-21.0.sql
@@ -0,0 +1,11 @@
+--liquibase formatted sql
+
+--changeset oldboy:1
+INSERT INTO cards (card_id, card_number, client_id, card_type, total_limit, amount_used, available_amount, create_dt)
+VALUES (1, '4565XXXX4656', 1, 'Credit', 10000, 500, 9500, NOW()),
+       (2, '3455XXXX8673', 1, 'Credit', 7500, 600, 6900, NOW()),
+       (3, '2359XXXX9346', 1, 'Credit', 20000, 4000, 16000, NOW()),
+       (4, '3365XXXX4886', 2, 'Credit', 5000, 300, 8500, NOW()),
+       (5, '5655XXXX8343', 2, 'Credit', 17500, 1600, 9900, NOW()),
+       (6, '7859XXXX9322', 2, 'Credit', 2000, 3200, 6000, NOW());
+--rollback delete from cards
