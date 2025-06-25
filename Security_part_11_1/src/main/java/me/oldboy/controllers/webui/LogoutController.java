f768ebb760d5717d51b7154adb76669df6c75819
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-25 16:30:55 +0500

    Security_part_11_1: add LogoutController.java

---
+++ Security_part_11_1/src/main/java/me/oldboy/controllers/webui/LogoutController.java
@@ -0,0 +1,25 @@
+package me.oldboy.controllers.webui;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/webui")
+@RequiredArgsConstructor
+public class LogoutController {
+
+    @PostMapping("/logout")
+    public String logOut(){
+        return "redirect:/webui/bye";
+    }
+
+    @GetMapping("/bye")
+    public String buy(){
+        return "/bye.html";
+    }
+}
