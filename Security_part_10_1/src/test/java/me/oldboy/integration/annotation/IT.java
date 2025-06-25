d8f5047e22b0de130e6c0611eca5f5a40683556b
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-25 12:57:55 +0500

    Security_part_10_1: add IT.java

---
+++ Security_part_10_1/src/test/java/me/oldboy/integration/annotation/IT.java
@@ -0,0 +1,24 @@
+package me.oldboy.integration.annotation;
+
+import me.oldboy.config.test_main.TestMainConfig;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@ActiveProfiles("test")
+@Transactional
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {TestMainConfig.class})
+@WebAppConfiguration
+public @interface IT {
+}
