4a8544de60ba67112b1804b6cb8ff8451177bcc7
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-25 16:29:00 +0500

    Security_part_11_1: add SecurityConstants.java

---
+++ Security_part_11_1/src/main/java/me/oldboy/constants/SecurityConstants.java
@@ -0,0 +1,8 @@
+package me.oldboy.constants;
+
+public class SecurityConstants {
+
+	public static final String JWT_KEY = "235kjBJHGJ9hlkLLbvc3fJJHLovrfdy7JHKVTYHk";
+	public static final String JWT_HEADER = "Authorization";
+	public static final String EMAIL_COOKIE = "Email";
+}
