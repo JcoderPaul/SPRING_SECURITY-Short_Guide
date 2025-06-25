6354a4c1c47485a676f2713142ddd9e148506122
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-25 12:58:49 +0500

    Security_part_10_1: add AdminControllerIT.java

---
+++ Security_part_10_1/src/test/java/me/oldboy/integration/controllers/api/AdminControllerIT.java
@@ -0,0 +1,80 @@
+package me.oldboy.integration.controllers.api;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import me.oldboy.config.test_data_source.TestContainerInit;
+import me.oldboy.dto.client_dto.ClientReadDto;
+import me.oldboy.integration.annotation.IT;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.List;
+
+import static me.oldboy.test_constant.TestConstantFields.EXIST_EMAIL;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@IT
+class AdminControllerIT extends TestContainerInit {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp(){
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    @SneakyThrows
+    @WithUserDetails(value = EXIST_EMAIL, userDetailsServiceBeanName = "clientDetailsService")
+    void getAllClient_ShouldReturnDtoList_AuthAdmin_Test() {
+        MvcResult result = mockMvc.perform(get("/api/admin/getAllClient"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String strResult = result.getResponse().getContentAsString();
+
+        List<ClientReadDto> listFromResponse = new ObjectMapper().readValue(strResult, new TypeReference<List<ClientReadDto>>() {});
+
+        assertThat(listFromResponse.size()).isGreaterThan(1);
+    }
+
+    @Test
+    @SneakyThrows
+    @WithUserDetails(value = "user@test.com", userDetailsServiceBeanName = "clientDetailsService")
+    void getAllClient_ShouldReturnOk_AuthRead_Test() {
+        MvcResult result = mockMvc.perform(get("/api/admin/getAllClient"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String strResult = result.getResponse().getContentAsString();
+
+        List<ClientReadDto> listFromResponse = new ObjectMapper().readValue(strResult, new TypeReference<List<ClientReadDto>>() {});
+
+        assertThat(listFromResponse.size()).isGreaterThan(1);
+    }
+
+    @Test
+    @SneakyThrows
+    @WithUserDetails(value = "user3@test.com", userDetailsServiceBeanName = "clientDetailsService")
+    void getAllClient_ShouldReturnForbidden_AuthNotAdminOrRead_Test() {
+        mockMvc.perform(get("/api/admin/getAllClient"))
+                .andExpect(status().isForbidden())
+                .andExpect(content().string(""));
+    }
+}
