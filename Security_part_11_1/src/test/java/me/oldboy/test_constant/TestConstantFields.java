21e6f89f1b907f0c1c245df583102cc5264a934a
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-27 19:29:15 +0500

    Security_part_11_1: add TestConstantFields.java

---
+++ Security_part_11_1/src/test/java/me/oldboy/test_constant/TestConstantFields.java
@@ -0,0 +1,16 @@
+package me.oldboy.test_constant;
+
+public class TestConstantFields {
+    public final static String TEST_EMAIL = "for_test@test.com";
+    public final static String TEST_PASS = "1234";
+    public final static String TEST_CLIENT_NAME = "Testino";
+    public final static String TEST_CLIENT_SUR_NAME = "Testorelly";
+    public final static Integer TEST_AGE = 32;
+    public final static String TEST_STR_ROLE_USER = "USER";
+    public final static String TEST_STR_ROLE_ADMIN = "ADMIN";
+
+    public final static String EXIST_EMAIL = "admin@test.com";
+    public final static String EXIST_EMAIL_WITH_READ_AUTH = "user@test.com";
+    public final static String NON_EXIST_EMAIL = "non_email@test.com";
+    public final static String WRONG_PASS = "4321";
+}
