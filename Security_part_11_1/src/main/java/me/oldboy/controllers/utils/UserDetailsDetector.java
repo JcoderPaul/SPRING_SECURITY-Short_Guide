7b37b52081142eb51bb2057cc70e779d0fc1f9a5
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-06-30 20:57:19 +0500

    Security_part_11_1: add UserDetailsDetector.java

---
+++ Security_part_11_1/src/main/java/me/oldboy/controllers/utils/UserDetailsDetector.java
@@ -0,0 +1,23 @@
+package me.oldboy.controllers.utils;
+
+import me.oldboy.models.client.Client;
+import me.oldboy.services.ClientService;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class UserDetailsDetector {
+    public Optional<Client> getClientFromBase(ClientService clientService, Authentication authentication){
+        String clientEmail;
+        Optional<Client> mayBeClient = Optional.empty();
+
+        if(authentication != null && clientService != null){
+            clientEmail = authentication.getName();
+            mayBeClient = clientService.findByEmail(clientEmail);
+        }
+
+        return mayBeClient;
+    }
+}
