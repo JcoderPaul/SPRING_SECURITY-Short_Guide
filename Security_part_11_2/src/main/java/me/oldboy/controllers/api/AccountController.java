80544358bb7eddf3a5ab9fac4564467530323e51
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-07-04 13:09:14 +0500

    Security_part_11_2: add AccountController.java

---
+++ Security_part_11_2/src/main/java/me/oldboy/controllers/api/AccountController.java
@@ -0,0 +1,51 @@
+package me.oldboy.controllers.api;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import me.oldboy.controllers.util.UserDetailsDetector;
+import me.oldboy.dto.account_dto.AccountReadDto;
+import me.oldboy.models.client.Client;
+import me.oldboy.services.AccountService;
+import me.oldboy.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@Slf4j
+@RestController
+@RequestMapping("/api")
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccountController {
+
+	@Autowired
+	private AccountService accountService;
+	@Autowired
+	private ClientService clientService;
+	@Autowired
+	private UserDetailsDetector userDetailsDetector;
+
+	@GetMapping("/myAccount")
+	public ResponseEntity<AccountReadDto> getAccountDetails() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		ResponseEntity<AccountReadDto> responseAccount = ResponseEntity.noContent().build();
+
+		Optional<Client> mayBeClient =userDetailsDetector.getClientFromBase(clientService,authentication);
+		if (mayBeClient.isPresent()) {
+			long clientId = mayBeClient.get().getId();
+
+			AccountReadDto mayBeAccount = accountService.readAccountByClientId(clientId);
+			if(mayBeAccount != null){
+				responseAccount = ResponseEntity.ok().body(mayBeAccount);
+			}
+		}
+		return responseAccount;
+	}
+}
