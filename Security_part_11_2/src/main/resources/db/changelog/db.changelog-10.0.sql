8bd0b98fa420ca04c99c1ab036829c242749220c
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-07-04 14:07:44 +0500

    Security_part_11_2: add db.changelog-10.0.sql

---
+++ Security_part_11_2/src/main/resources/db/changelog/db.changelog-10.0.sql
@@ -0,0 +1,11 @@
+--liquibase formatted sql
+
+--changeset oldboy:1
+INSERT INTO clients_authorities (id, client_id, authority_id)
+VALUES (1, 1, 5),
+       (2, 2, 2),
+       (3, 2, 3),
+       (4, 3, 1),
+       (5, 3, 4),
+       (6, 1, 1);
+--rollback delete from clients_authorities
