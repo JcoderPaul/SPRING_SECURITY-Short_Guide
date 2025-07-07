056ed699e85fb9297e2841135373c16b152c0717
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-07-07 06:01:57 +0500

    Security_part_11_3: add ClientMapper.java

---
+++ Security_part_11_3/src/main/java/me/oldboy/mapper/ClientMapper.java
@@ -0,0 +1,23 @@
+package me.oldboy.mapper;
+
+import me.oldboy.dto.client_dto.ClientCreateDto;
+import me.oldboy.dto.client_dto.ClientReadDto;
+import me.oldboy.models.client.Client;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface ClientMapper {
+
+    ClientMapper INSTANCE = Mappers.getMapper(ClientMapper.class);
+
+    @Mapping(target = "clientName", source = "client.details.clientName")
+    @Mapping(target = "clientSurName", source = "client.details.clientSurName")
+    @Mapping(target = "age", source = "client.details.age")
+    ClientReadDto mapToClientReadDto(Client client);
+
+    @Mapping(target = "details", source = "details")
+    Client mapToClient(ClientCreateDto clientCreateDto);
+}
