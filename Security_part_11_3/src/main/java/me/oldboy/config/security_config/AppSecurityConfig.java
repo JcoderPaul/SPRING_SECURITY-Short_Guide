30dd03b41b6c82fcbc42571d927e7b637f130aa3
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-07-07 05:46:34 +0500

    Security_part_11_3: add AppSecurityConfig.java

---
+++ Security_part_11_3/src/main/java/me/oldboy/config/security_config/AppSecurityConfig.java
@@ -0,0 +1,25 @@
+package me.oldboy.config.security_config;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity(debug = true) // позволяет фиксировать происходящее в режиме отладки
+@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+@ComponentScan("me.oldboy.config.main_config")
+public class AppSecurityConfig {
+
+	@Bean
+	@SneakyThrows
+	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) {
+		return FilterChainConfig.getSecurityFilterChain(httpSecurity);
+	}
+}
