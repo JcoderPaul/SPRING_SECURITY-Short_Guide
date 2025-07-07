c1bb87e38ccbda438498a592f1dde1cb7aa80ec8
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-07-07 05:58:40 +0500

    Security_part_11_3: add DetailsCreateDto.java

---
+++ Security_part_11_3/src/main/java/me/oldboy/dto/details_dto/DetailsCreateDto.java
@@ -0,0 +1,17 @@
+package me.oldboy.dto.details_dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+
+@Builder
+public record DetailsCreateDto(@NotBlank
+                               @Size(min = 2, max = 64, message = "Name field size can be between 2 and 64")
+                               String clientName,
+                               @NotBlank
+                               @Size(min = 2, max = 64, message = "Surname field size can be between 2 and 64")
+                               String clientSurName,
+                               @Positive(message = "Age can't be lass then 0, unless you come from a counter-directional universe!")
+                               Integer age) {
+}
