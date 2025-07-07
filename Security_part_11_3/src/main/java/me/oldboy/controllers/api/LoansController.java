8e0b54eb602c796aa1e0dd2b4e63a60a31ca94f5
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-07-07 05:51:29 +0500

    Security_part_11_3: add LoansController.java

--- Security_part_11_3/src/main/java/me/oldboy/controllers/api/LoansController.java
+++ Security_part_11_3/src/main/java/me/oldboy/controllers/api/LoansController.java
@@ -1,11 +1,8 @@
 package me.oldboy.controllers.api;
 
-import jakarta.annotation.security.RolesAllowed;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import me.oldboy.config.securiry_details.SecurityClientDetails;
 import me.oldboy.controllers.util.UserDetailsDetector;
 import me.oldboy.dto.loan_dto.LoanCreateDto;
 import me.oldboy.dto.loan_dto.LoanReadDto;
@@ -15,36 +12,36 @@ import me.oldboy.services.LoanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PostFilter;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.access.prepost.PreFilter;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
-@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
 @RequestMapping("/api")
 public class LoansController {
 
 	@Autowired
-	private final ClientService clientService;
+	private ClientService clientService;
 	@Autowired
-	private final LoanService loanService;
+	private LoanService loanService;
 	@Autowired
-	private final UserDetailsDetector userDetailsDetector;
-
+	private UserDetailsDetector userDetailsDetector;
+	
 	@GetMapping("/myLoans")
 	public ResponseEntity<List<LoanReadDto>> getLoanDetails() {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		ResponseEntity<List<LoanReadDto>> loansList = ResponseEntity.noContent().build();
 
-		Optional<Client> mayBeClient = userDetailsDetector.getClientFromBase(clientService,authentication);
+		Optional<Client> mayBeClient = userDetailsDetector.getClientFromBase(clientService, authentication);
+
 		if(mayBeClient.isPresent()){
 			long clientId = mayBeClient.get().getId();
 
@@ -56,40 +53,49 @@ public class LoansController {
 		return loansList;
 	}
 
-	@PreAuthorize("#loanCreateDto.clientId == #userDetails.client.id")
 	@PostMapping("/createLoan")
-	public ResponseEntity<?> createLoan(@Param("loanCreateDto") @RequestBody LoanCreateDto loanCreateDto,
-										@Param("userDetails") @AuthenticationPrincipal SecurityClientDetails userDetails){
-
-		String userEmail = userDetails.getUsername();
+	public ResponseEntity<?> createLoan(@Param("loanCreateDto") @RequestBody LoanCreateDto loanCreateDto) {
+		String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
 		Long newLoanId = null;
 
 		Optional<Client> mayBeClient = clientService.findByEmail(userEmail);
 		if(mayBeClient.isPresent()){
-			loanCreateDto.setClientId(mayBeClient.get().getId());
-			newLoanId = loanService.saveLoan(loanCreateDto);
+			if(mayBeClient.get().getId() == loanCreateDto.getClientId()){
+				newLoanId = loanService.saveLoan(loanCreateDto);
+				return ResponseEntity.ok().body(newLoanId);
+			}
 		}
-		return ResponseEntity.ok().body(newLoanId);
+		return ResponseEntity.badRequest().body("");
 	}
 
-	@PreFilter(filterTarget = "dtoList", value = "filterObject.clientId == #userDetails.client.id")
 	@PostMapping("/save-all-loans")
-	public ResponseEntity<String> saveAllMyRequestLoan(@Param("dtoList") @RequestBody List<LoanCreateDto> dtoList,
-									              @Param("userDetails") @AuthenticationPrincipal SecurityClientDetails userDetails){
-
+	public ResponseEntity<String> saveAllMyRequestLoan(@Param("dtoList") @RequestBody List<LoanCreateDto> dtoList){
 		String prnResponseAsSting = "Operation is failed!";
-		if(loanService.saveAllMyLoans(dtoList, userDetails)){
+		List<LoanCreateDto> toSaveList = null;
+
+		String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
+		Optional<Client> mayBeClient = clientService.findByEmail(userEmail);
+
+		if(mayBeClient.isPresent()){
+			long clientId = mayBeClient.get().getId();
+			toSaveList = dtoList.stream()
+					.filter(loanCreateDto -> loanCreateDto.getClientId() == clientId)
+					.toList();
+		}
+
+		if(loanService.saveAllMyLoans(toSaveList)){
 			prnResponseAsSting = "Saved all loans!";
 		}
 
 		return ResponseEntity.ok().body(prnResponseAsSting);
 	}
 
-	@RolesAllowed("ADMIN") // Эта аннотация из раздела о защите по правам доступа, теперь метод могут использовать только ADMIN-ы
-	@PostFilter("filterObject.loanType == #loanType")
+	@Secured("ROLE_ADMIN")
 	@GetMapping("/get-all-loans-by-type/{loanType}")
 	public List<LoanReadDto> getAllLoanByType(@Param("loanType") @PathVariable("loanType") String loanType){
-		List<LoanReadDto> returnDtoList = loanService.findAll();
-		return returnDtoList;
+		List<LoanReadDto> getLoansFromBase = loanService.findAll();
+		return getLoansFromBase.stream()
+				.filter(loanReadDto -> loanReadDto.getLoanType().equals(loanType))
+				.collect(Collectors.toList());
 	}
 }
