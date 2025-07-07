ad23542ee9e690ed840573b93332591514724607
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-07-07 06:08:08 +0500

    Security_part_11_3: add DetailsValidator.java

---
+++ Security_part_11_3/src/main/java/me/oldboy/validation/validator/DetailsValidator.java
@@ -0,0 +1,26 @@
+package me.oldboy.validation.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import me.oldboy.dto.details_dto.DetailsCreateDto;
+import me.oldboy.validation.annitation.CheckDetails;
+
+public class DetailsValidator implements ConstraintValidator<CheckDetails, DetailsCreateDto> {
+
+    @Override
+    public void initialize(CheckDetails constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(DetailsCreateDto enteredDetail, ConstraintValidatorContext context) {
+        boolean isNameNotBlankAndMoreThen2Char = enteredDetail.clientSurName().trim().length() > 2;
+        boolean isSurNameNotBlankAndMoreThen2Char = enteredDetail.clientName().trim().length() > 2;
+        boolean isAgeMoreThenZero = enteredDetail.age() >= 0;
+        if (isAgeMoreThenZero && isNameNotBlankAndMoreThen2Char && isSurNameNotBlankAndMoreThen2Char) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
