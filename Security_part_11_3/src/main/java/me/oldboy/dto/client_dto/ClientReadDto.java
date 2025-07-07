735891590791cd3d34d96f4cfc624ce2918bf905
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-07-07 05:58:18 +0500

    Security_part_11_3: add ClientReadDto.java

---
+++ Security_part_11_3/src/main/java/me/oldboy/dto/client_dto/ClientReadDto.java
@@ -0,0 +1,26 @@
+package me.oldboy.dto.client_dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class ClientReadDto {
+    private Long id;
+    private String email;
+    private String role;
+    private String clientName;
+    private String clientSurName;
+    private Integer age;
+
+    @Override
+    public String toString() {
+        return " Email: " + email +
+               " Role: " + role +
+               " Name: " + clientName +
+               " Surname: " + clientSurName +
+               " Age: " + age;
+    }
+}
