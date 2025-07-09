1c748b30b513d0a729b771da013bbe966495d577
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-07-09 07:08:04 +0500

    Security_part_11_3: update AppSecurityConfig.java

--- Security_part_11_3/src/main/java/me/oldboy/config/security_config/AppSecurityConfig.java
+++ Security_part_11_3/src/main/java/me/oldboy/config/security_config/AppSecurityConfig.java
@@ -1,13 +1,18 @@
 package me.oldboy.config.security_config;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Slf4j
@@ -15,11 +20,18 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity(debug = true) // позволяет фиксировать происходящее в режиме отладки
 @EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 @ComponentScan("me.oldboy.config.main_config")
+@AllArgsConstructor
+@NoArgsConstructor
 public class AppSecurityConfig {
+
+	@Autowired
+	private JwtDecoder jwtDecoder;
+	@Autowired
+	private JwtAuthenticationConverter jwtAuthenticationConverter;
 
 	@Bean
 	@SneakyThrows
 	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) {
-		return FilterChainConfig.getSecurityFilterChain(httpSecurity);
+		return FilterChainConfig.getSecurityFilterChain(httpSecurity, jwtDecoder, jwtAuthenticationConverter);
 	}
 }
