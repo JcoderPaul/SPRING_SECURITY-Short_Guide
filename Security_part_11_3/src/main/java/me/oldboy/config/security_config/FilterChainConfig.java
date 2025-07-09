9d20be75db8e838be900c0b762637807b493555f
Paul <100008373+JcoderPaul@users.noreply.github.com>
2025-07-09 07:08:24 +0500

    Security_part_11_3: update FilterChainConfig.java

--- Security_part_11_3/src/main/java/me/oldboy/config/security_config/FilterChainConfig.java
+++ Security_part_11_3/src/main/java/me/oldboy/config/security_config/FilterChainConfig.java
@@ -1,26 +1,29 @@
 package me.oldboy.config.security_config;
 
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
-
 public class FilterChainConfig {
-    public static SecurityFilterChain getSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    public static SecurityFilterChain getSecurityFilterChain(HttpSecurity httpSecurity,
+                                                             JwtDecoder jwtDecoder,
+                                                             JwtAuthenticationConverter jwtAuthenticationConverter) throws Exception {
         httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable)
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)
+                                .decoder(jwtDecoder)))
                 .authorizeHttpRequests(urlConfig -> urlConfig
-                        .requestMatchers(antMatcher("/api/regClient")).permitAll()
-                        .requestMatchers(antMatcher("/api/myAccount"),
-                                antMatcher("/api/myBalance"),
-                                antMatcher("/api/myContact"),
-                                antMatcher("/api/myLoans")).authenticated()
-                        .requestMatchers(antMatcher("/api/myCards"),
-                                antMatcher("/api/admin/**")).hasRole("ADMIN")
+                        .requestMatchers("/api/regClient").permitAll()
+                        .requestMatchers("/api/myAccount",
+                                "/api/myBalance",
+                                "/api/myContact",
+                                "/api/myLoans").authenticated()
+                        .requestMatchers("/api/myCards",
+                                "/api/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated());
 
         return httpSecurity.build();
